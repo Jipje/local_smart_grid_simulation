d2b7886a5796dd299521cbc63104ef1b91fab40d
Jip Rietveld <jip_rietveld@hotmail.com>
2021-11-23 16:32:54 +0100

    Readjusted the lines and added documentation

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ def run_simulation(starting_time_step=0, number_of_steps=100, scenario='data/ten
     with open(scenario, 'r') as read_obj:
         csv_reader = reader(read_obj)
         steps_taken = 0
+        # Open the scenario
         for environment_data in csv_reader:
-            if starting_time_step < 0:
-                if steps_taken >= number_of_steps:
+            if starting_time_step >= 0:  # Skip lines until we reach the starting step.
+                starting_time_step = starting_time_step - 1
+            else:
+                if steps_taken >= number_of_steps:  # If we reach our maximum amount of steps. Stop the simulation
                     break
                 else:
+                    # The environment should take a step here.
                     rhino.take_action()
+                    print(environment_data)
                 steps_taken = steps_taken + 1
-            else:
-                starting_time_step = starting_time_step - 1
+
     print('END OF SIMULATION, TOOK {} STEPS'.format(steps_taken))
 
 
 if __name__ == '__main__':
-    run_simulation(100000, 100)
+    run_simulation(0, 100)
