47b16d8e808f26c68385baff6ce44cb5875d5e4e
Jip Rietveld <jip_rietveld@hotmail.com>
2021-11-23 16:15:03 +0100

    Introduced the CSV and market data is now available

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+from csv import reader
 from Battery import Battery
 
 
-def run_simulation(number_of_steps=100):
+def run_simulation(starting_time_step=0, number_of_steps=100, scenario='data/tennet_balans_delta_nov_2020_nov_2021.csv'):
     rhino = Battery('Rhino', 7500, 12000, 50)
-    for _ in range(number_of_steps):
-        rhino.take_action()
-    print('END OF SIMULATION')
+
+    # open file in read mode
+    with open(scenario, 'r') as read_obj:
+        csv_reader = reader(read_obj)
+        steps_taken = 0
+        for environment_data in csv_reader:
+            if starting_time_step < 0:
+                if steps_taken >= number_of_steps:
+                    break
+                else:
+                    rhino.take_action()
+                steps_taken = steps_taken + 1
+            else:
+                starting_time_step = starting_time_step - 1
+    print('END OF SIMULATION, TOOK {} STEPS'.format(steps_taken))
 
 
 if __name__ == '__main__':
-    run_simulation()
+    run_simulation(100000, 100)
