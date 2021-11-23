9e59621d67749efc809f8da4d0b108eaa924aabd
Jip Rietveld <jip_rietveld@hotmail.com>
2021-11-23 16:41:47 +0100

    Interpret imbalance prices

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from csv import reader
 from Battery import Battery
+from ImbalanceMessageInterpreter import ImbalanceMessageInterpreter
 
 
 def run_simulation(starting_time_step=0, number_of_steps=100, scenario='data/tennet_balans_delta_nov_2020_nov_2021.csv'):
@@ -9,6 +10,8 @@ def run_simulation(starting_time_step=0, number_of_steps=100, scenario='data/ten
     with open(scenario, 'r') as read_obj:
         csv_reader = reader(read_obj)
         steps_taken = 0
+        imbalance_msg_interpreter = ImbalanceMessageInterpreter()
+
         # Open the scenario
         for environment_data in csv_reader:
             if starting_time_step >= 0:  # Skip lines until we reach the starting step.
@@ -18,12 +21,17 @@ def run_simulation(starting_time_step=0, number_of_steps=100, scenario='data/ten
                     break
                 else:
                     # The environment should take a step here.
+                    try:
+                        imbalance_msg_interpreter.update(float(environment_data[2]), float(environment_data[1]), float(environment_data[3]))
+                    except OverflowError:
+                        imbalance_msg_interpreter.reset()
+                        imbalance_msg_interpreter.update(float(environment_data[2]), float(environment_data[1]), float(environment_data[3]))
                     rhino.take_action()
-                    print(environment_data)
+                    print(imbalance_msg_interpreter.get_current_price())
                 steps_taken = steps_taken + 1
 
     print('END OF SIMULATION, TOOK {} STEPS'.format(steps_taken))
 
 
 if __name__ == '__main__':
-    run_simulation(0, 100)
+    run_simulation(100000, 100)
