d9d47b00153c5d1f368f17aafac6690a1d052a91
Jip Rietveld <jip_rietveld@hotmail.com>
2021-11-24 16:02:59 +0100

    SoC bandwidth set to 100 steps

--- StrategyBattery.py
+++ StrategyBattery.py
@@ -5,6 +5,7 @@ class StrategyBattery(object):
     def __init__(self):
         self.dayhead_tracker = False
         self.uploaded = False
+        self.strategy_matrix = []
 
     def upload_strategy(self, strategy_csv):
         strategy_df = pd.read_csv(strategy_csv)
@@ -16,7 +17,7 @@ class StrategyBattery(object):
 
         num_of_price_buckets = int((highest_price - lowest_price) / 5)
         strategy_matrix = []
-        for i in range(20):
+        for i in range(100):
             strategy_matrix.append([])
             for _ in range(num_of_price_buckets):
                 strategy_matrix[i].append('WAIT')
@@ -24,12 +25,12 @@ class StrategyBattery(object):
         for _, strategy_line in strategy_df.iterrows():
             current_soc = strategy_line.state_from
             while current_soc < strategy_line.state_until:
-                current_soc_index = int(current_soc / 5)
+                current_soc_index = current_soc
                 for current_price in range(lowest_price, highest_price, 5):
                     if strategy_line.price_from <= current_price <= strategy_line.price_until:
                         current_price_index = int(current_price / 5)
                         strategy_matrix[current_soc_index][current_price_index] = strategy_line.command
-                current_soc += 5
+                current_soc += 1
 
         self.uploaded = True
         self.strategy_matrix = strategy_matrix
@@ -43,7 +44,7 @@ class StrategyBattery(object):
         if discharge_price < 0:
             discharge_price = 0
 
-        soc_index = int(state_of_charge / 5)
+        soc_index = int(state_of_charge)
         charge_price_index = int(charge_price / 5)
         discharge_price_index = int(discharge_price / 5)
 
