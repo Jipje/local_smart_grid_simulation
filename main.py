dbd57c8edfef07a9fecc746ecd2d97f81e149e2e
Jip Rietveld <jip_rietveld@hotmail.com>
2021-11-26 09:28:29 +0100

    Introduce random selection of a month

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from csv import reader
 from Battery import Battery
-from StrategyBattery import StrategyBattery
 from ImbalanceMessageInterpreter import ImbalanceMessageInterpreter
+import random
+
+
+def run_random_month(scenario='data/tennet_balans_delta_nov_2020_nov_2021.csv', verbose_lvl=2):
+    start_day = random.randint(0, 333)
+    starting_timestep = start_day * 24 * 60
+    number_of_steps = 30 * 24 * 60
+    run_simulation(starting_timestep, number_of_steps, scenario=scenario, verbose_lvl=verbose_lvl)
 
 
 def run_simulation(starting_time_step=0, number_of_steps=100, scenario='data/tennet_balans_delta_nov_2020_nov_2021.csv', verbose_lvl=3):
@@ -41,4 +48,5 @@ def run_simulation(starting_time_step=0, number_of_steps=100, scenario='data/ten
 
 
 if __name__ == '__main__':
-    run_simulation(1440, 1440, verbose_lvl=2)
+    # run_simulation(1440, 1440, verbose_lvl=2)
+    run_random_month(verbose_lvl=2)
