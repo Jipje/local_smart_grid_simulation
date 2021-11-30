40e5f3350ac527f14efd809b03ce86e16443bb85
Jip Rietveld <jip_rietveld@hotmail.com>
2021-11-30 12:51:38 +0100

    Windnet data zip

--- windnet_zip.py
+++ windnet_zip.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from csv import writer
 
 if __name__ == '__main__':
     tennet_delta_df = pd.read_csv('data/tennet_balans_delta_nov_2020_nov_2021.csv')
@@ -14,4 +15,21 @@ if __name__ == '__main__':
     res_df = pd.merge(tennet_delta_df, windnet_df, left_index=True, right_index=True, how='inner')
     # print(res_df)
 
-    res_df.to_csv('data/tennet_balans_delta_and_trivial_windnet.csv')
+    with open('data/tennet_balans_delta_and_trivial_windnet.csv','w+', newline='') as new_file:
+        csv_writer = writer(new_file)
+        header = ['time'] + list(res_df.columns)
+        csv_writer.writerow(header)
+        tracker = 0
+
+        for index, row in res_df.iterrows():
+            csv_row = []
+
+            index_str = index.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+            csv_row = [index_str, row['tennet_balansdelta.mean_max_price'], row['tennet_balansdelta.mean_mid_price'],
+                       row['tennet_balansdelta.mean_min_price'], row['neushoorntocht_consumed_kw'],
+                       row['neushoorntocht_produced_kw'], row['mammoettocht_consumed_kw'], row['mammoettocht_produced_kw']]
+            csv_writer.writerow(csv_row)
+
+            if tracker % 2020 == 0:
+                print(csv_row)
+            tracker += 1
