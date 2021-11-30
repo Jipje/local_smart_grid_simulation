d1517a192537ab8c0dd3d3825746ecb6f24cee9f
Jip Rietveld <jip_rietveld@hotmail.com>
2021-11-30 10:41:59 +0100

    Create csv line

--- windnet_csv_translation.py
+++ windnet_csv_translation.py
@@ -24,19 +24,15 @@ if __name__ == '__main__':
             end_of_5m_interval = dt.datetime.strptime(wind_net_data[0], '%d/%m/%Y %H:%M').replace(tzinfo=ams)
             end_of_5m_interval = end_of_5m_interval.astimezone(utc)
             start_of_5m_interval = end_of_5m_interval - dt.timedelta(minutes=5)
-            print(start_of_5m_interval)
-            print(end_of_5m_interval)
 
             time_tracker = start_of_5m_interval
-            neushoorntocht_consumed_kw = trivial_kw_per_minute(float(wind_net_data[1]))
-            neushoorntocht_produced_kw = trivial_kw_per_minute(float(wind_net_data[2]))
-            mammoettocht_consumed_kw = trivial_kw_per_minute(float(wind_net_data[3]))
-            mammoettocht_produced_kw = trivial_kw_per_minute(float(wind_net_data[4]))
+            neushoorntocht_consumed_kw = str(trivial_kw_per_minute(float(wind_net_data[1])))
+            neushoorntocht_produced_kw = str(trivial_kw_per_minute(float(wind_net_data[2])))
+            mammoettocht_consumed_kw = str(trivial_kw_per_minute(float(wind_net_data[3])))
+            mammoettocht_produced_kw = str(trivial_kw_per_minute(float(wind_net_data[4])))
             while time_tracker < end_of_5m_interval:
-                print(time_tracker)
-                print(neushoorntocht_consumed_kw)
-                print(neushoorntocht_produced_kw)
-                print(mammoettocht_consumed_kw)
-                print(mammoettocht_produced_kw)
+                time_tracker_str = time_tracker.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+                csv_line = time_tracker_str + ',' + neushoorntocht_consumed_kw + ',' + neushoorntocht_produced_kw + ',' + mammoettocht_consumed_kw + ',' + mammoettocht_produced_kw
+                print(csv_line)
                 time_tracker += dt.timedelta(minutes=1)
             break
