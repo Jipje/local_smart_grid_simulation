26dce2f43f49f1b53d2aae1c232d390d2be1cf85
Jip Rietveld <jip_rietveld@hotmail.com>
2021-11-30 10:27:03 +0100

    Read and print single line

--- windnet_csv_translation.py
+++ windnet_csv_translation.py
@@ -1,4 +1,9 @@
 from csv import reader
+import datetime as dt
+import dateutil.tz
+
+ams = dateutil.tz.gettz('Europe/Amsterdam')
+utc = dateutil.tz.tzutc()
 
 if __name__ == '__main__':
     with open('data/wind_net_csv.csv', 'r') as read_obj:
@@ -8,5 +13,13 @@ if __name__ == '__main__':
             if not header:
                 header = True
                 continue
-            print(wind_net_data)
+            end_of_5m_interval = dt.datetime.strptime(wind_net_data[0], '%d/%m/%Y %H:%M').replace(tzinfo=ams)
+            end_of_5m_interval = end_of_5m_interval.astimezone(utc)
+            start_of_5m_interval = end_of_5m_interval - dt.timedelta(minutes=5)
+            print(start_of_5m_interval)
+            print(end_of_5m_interval)
+            neushoorntocht_produced_kwh = float(wind_net_data[2])
+            print(neushoorntocht_produced_kwh)
+            mammoettocht_produced_kwh = float(wind_net_data[4])
+            print(mammoettocht_produced_kwh)
             break
