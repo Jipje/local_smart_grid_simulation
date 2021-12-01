0007aab8707a2201ade866a656a3b66072c11223
Jip Rietveld <jip_rietveld@hotmail.com>
2021-12-01 10:39:13 +0100

    Moved modified windnet_zip file

--- one_time_scripts/windnet_zip.py
+++ one_time_scripts/windnet_zip.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     tennet_delta_df = tennet_delta_df.dropna()
     # print(tennet_delta_df)
 
-    windnet_df = pd.read_csv('data/windnet/trivial_cleaned_windnet_data_sep_2020_sep_2021.csv')
+    windnet_df = pd.read_csv('../data/windnet/trivial_cleaned_windnet_data_sep_2020_sep_2021.csv')
     windnet_df.index = pd.to_datetime(windnet_df['time'], errors='coerce')
     windnet_df = windnet_df.drop('time', axis=1)
     # print(windnet_df)
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     res_df = pd.merge(tennet_delta_df, windnet_df, left_index=True, right_index=True, how='inner')
     # print(res_df)
 
-    with open('data/tennet_and_windnet/larger_tennet_balans_delta_and_trivial_windnet.csv', 'w+', newline='') as new_file:
+    with open('../data/tennet_and_windnet/larger_tennet_balans_delta_and_trivial_windnet.csv', 'w+', newline='') as new_file:
         csv_writer = writer(new_file)
         header = ['time'] + list(res_df.columns)
         csv_writer.writerow(header)
