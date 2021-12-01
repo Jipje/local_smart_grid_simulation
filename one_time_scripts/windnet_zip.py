1cf41305ef2efa2a12029d0bd723b0bbf62ecba1
Jip Rietveld <jip_rietveld@hotmail.com>
2021-12-01 10:47:20 +0100

    Removed faulty tennet_balans_delta information

--- one_time_scripts/windnet_zip.py
+++ one_time_scripts/windnet_zip.py
@@ -2,7 +2,7 @@ import pandas as pd
 from csv import writer
 
 if __name__ == '__main__':
-    tennet_delta_df = pd.read_csv('data/tennet_balans_delta_sep_2020_sep_2021.csv')
+    tennet_delta_df = pd.read_csv('../data/tennet_balans_delta/tennet_balans_delta_sep_2020_sep_2021.csv')
     tennet_delta_df.index = pd.to_datetime(tennet_delta_df['time'], errors='coerce')
     tennet_delta_df = tennet_delta_df.drop('time', axis=1)
     tennet_delta_df = tennet_delta_df.dropna()
@@ -27,19 +27,11 @@ if __name__ == '__main__':
 
             index_str = index.strftime('%Y-%m-%dT%H:%M:%S.000Z')
 
-            tennet_max = row['tennet_balansdelta.mean_max_price']
-            if tennet_max == 0.0:
-                tennet_max = -99999
-
-            tennet_min = row['tennet_balansdelta.mean_min_price']
-            if tennet_min == 0.0:
-                tennet_min = 99999
-
-            csv_row = [index_str, tennet_max, row['tennet_balansdelta.mean_mid_price'],
-                       tennet_min, row['neushoorntocht_consumed_kw'],
+            csv_row = [index_str, row['tennet_balansdelta.mean_max_price'], row['tennet_balansdelta.mean_mid_price'],
+                       row['tennet_balansdelta.mean_min_price'], row['neushoorntocht_consumed_kw'],
                        row['neushoorntocht_produced_kw'], row['mammoettocht_consumed_kw'], row['mammoettocht_produced_kw']]
             csv_writer.writerow(csv_row)
 
-            if tracker % 2020 == 0:
+            if tracker % 202020 == 0:
                 print(csv_row)
             tracker += 1
