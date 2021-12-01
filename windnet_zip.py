2f31a2285b89a0e8225d5b35b3e88d59968a6739
Jip Rietveld <jip_rietveld@hotmail.com>
2021-12-01 10:34:37 +0100

    Zip new imbalance data

--- windnet_zip.py
+++ windnet_zip.py
@@ -2,7 +2,7 @@ import pandas as pd
 from csv import writer
 
 if __name__ == '__main__':
-    tennet_delta_df = pd.read_csv('data/tennet_balans_delta_nov_2020_nov_2021.csv')
+    tennet_delta_df = pd.read_csv('data/tennet_balans_delta_sep_2020_sep_2021.csv')
     tennet_delta_df.index = pd.to_datetime(tennet_delta_df['time'], errors='coerce')
     tennet_delta_df = tennet_delta_df.drop('time', axis=1)
     tennet_delta_df = tennet_delta_df.dropna()
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     res_df = pd.merge(tennet_delta_df, windnet_df, left_index=True, right_index=True, how='inner')
     # print(res_df)
 
-    with open('data/tennet_balans_delta_and_trivial_windnet.csv','w+', newline='') as new_file:
+    with open('data/larger_tennet_balans_delta_and_trivial_windnet.csv','w+', newline='') as new_file:
         csv_writer = writer(new_file)
         header = ['time'] + list(res_df.columns)
         csv_writer.writerow(header)
@@ -26,8 +26,17 @@ if __name__ == '__main__':
             csv_row = []
 
             index_str = index.strftime('%Y-%m-%dT%H:%M:%S.000Z')
-            csv_row = [index_str, row['tennet_balansdelta.mean_max_price'], row['tennet_balansdelta.mean_mid_price'],
-                       row['tennet_balansdelta.mean_min_price'], row['neushoorntocht_consumed_kw'],
+
+            tennet_max = row['tennet_balansdelta.mean_max_price']
+            if tennet_max == 0.0:
+                tennet_max = -99999
+
+            tennet_min = row['tennet_balansdelta.mean_min_price']
+            if tennet_min == 0.0:
+                tennet_min = 99999
+
+            csv_row = [index_str, tennet_max, row['tennet_balansdelta.mean_mid_price'],
+                       tennet_min, row['neushoorntocht_consumed_kw'],
                        row['neushoorntocht_produced_kw'], row['mammoettocht_consumed_kw'], row['mammoettocht_produced_kw']]
             csv_writer.writerow(csv_row)
 
