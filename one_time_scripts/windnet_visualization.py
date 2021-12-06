56aa46433d08687e26dec79a5929e2f6569ac71b
Jip Rietveld <jip_rietveld@hotmail.com>
2021-12-06 09:49:34 +0100

    Added scatter plots of 100 largest moments

--- one_time_scripts/windnet_visualization.py
+++ one_time_scripts/windnet_visualization.py
@@ -18,6 +18,8 @@ if __name__ == '__main__':
     windnet_df.index = pd.to_datetime(windnet_df['date'], errors='coerce', utc=True)
 
     windnet_df['nht_production_kw'] = windnet_df['nht_production_kwh'] / 5 * 60
+    windnet_df['hour_of_production'] = windnet_df.index.hour
+    windnet_df['minute_of_production'] = windnet_df.index.minute
 
     print(windnet_df)
 
@@ -29,7 +31,7 @@ if __name__ == '__main__':
 
     plt.hist(windnet_df['nht_production_kwh'], bins=100)
     plt.ylim(0, 200)
-    plt.ylabel('Number of occurences')
+    plt.ylabel('Number of occurrences')
     plt.xlabel('Generated power in 5m (kWh)')
     plt.title('Histogram of generated power by wind farm Neushoorntocht')
     plt.show()
@@ -37,9 +39,23 @@ if __name__ == '__main__':
     plt.hist(windnet_df['nht_production_kw'], bins=100)
     plt.ylim(0, 200)
     plt.axvline(x=22630, color='red')
-    plt.ylabel('Number of occurences')
+    plt.ylabel('Number of occurrences')
     plt.xlabel('Average Generated power per 5m (kW)')
     plt.title('Histogram of generated power by wind farm Neushoorntocht')
     plt.show()
 
-    print(windnet_df['nht_production_kw'].nlargest(100))
+    congestion_windnet_df = windnet_df.nlargest(n=100, columns='nht_production_kw')
+
+    plt.scatter(congestion_windnet_df['hour_of_production'], congestion_windnet_df['nht_production_kw'])
+    plt.ylabel('Average Generated power per 5m (kW)')
+    plt.xlabel('Hour in which power was generated')
+    plt.title('Scatterplot of generated power by wind farm Neushoorntocht')
+    plt.show()
+
+    plt.scatter(congestion_windnet_df['minute_of_production'], congestion_windnet_df['nht_production_kw'])
+    plt.ylabel('Average Generated power per 5m (kW)')
+    plt.xlabel('Minute in which power was generated')
+    plt.title('Scatterplot of generated power by wind farm Neushoorntocht')
+    plt.show()
+
+
