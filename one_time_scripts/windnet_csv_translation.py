79784be6b9bf27bdfb48bb5c3cd61c0409d8ed00
Jip Rietveld <jip_rietveld@hotmail.com>
2021-12-09 09:55:32 +0100

    Adjusted function to return a list

--- one_time_scripts/windnet_csv_translation.py
+++ one_time_scripts/windnet_csv_translation.py
@@ -12,7 +12,11 @@ def trivial_kw_per_minute(total_kwh, number_of_minutes=5):
     kwh_per_minute = total_kwh / number_of_minutes
     kw_per_minute = kwh_per_minute * 60
     kw_per_minute = round(kw_per_minute, 2)
-    return kw_per_minute
+
+    res = []
+    for i in range(number_of_minutes):
+        res.append(kw_per_minute)
+    return res
 
 
 def make_trivial_windnet_csv():
@@ -60,29 +64,26 @@ def make_smarter_windnet_csv():
             end_of_5m_interval = end_of_5m_interval.astimezone(utc)
             start_of_5m_interval = end_of_5m_interval - dt.timedelta(minutes=5)
 
-            time_tracker = start_of_5m_interval
             neushoorntocht_consumed_kw = trivial_kw_per_minute(float(wind_net_data[1]))
             neushoorntocht_produced_kw = trivial_kw_per_minute(float(wind_net_data[2]))
             mammoettocht_consumed_kw = trivial_kw_per_minute(float(wind_net_data[3]))
             mammoettocht_produced_kw = trivial_kw_per_minute(float(wind_net_data[4]))
 
-            while time_tracker < end_of_5m_interval:
+            for i in range(5):
+                time_tracker = start_of_5m_interval + dt.timedelta(minutes=i)
                 time_tracker_str = time_tracker.strftime('%Y-%m-%dT%H:%M:%S.000Z')
                 new_row = {
                     'time': time_tracker_str,
-                    'neushoorntocht_consumed_kw': neushoorntocht_consumed_kw,
-                    'neushoorntocht_produced_kw': neushoorntocht_produced_kw,
-                    'mammoettocht_consumed_kw': mammoettocht_consumed_kw,
-                    'mammoettocht_produced_kw': mammoettocht_produced_kw
+                    'neushoorntocht_consumed_kw': neushoorntocht_consumed_kw[i],
+                    'neushoorntocht_produced_kw': neushoorntocht_produced_kw[i],
+                    'mammoettocht_consumed_kw': mammoettocht_consumed_kw[i],
+                    'mammoettocht_produced_kw': mammoettocht_produced_kw[i]
                 }
                 res_list.append(new_row)
-                if time_tracker.hour == 20 and time_tracker.minute == 20:
-                    print(new_row)
                 counter += 1
-                time_tracker += dt.timedelta(minutes=1)
             if counter >= 1440:
                 break
-    res_df = pd.DataFrame.from_dict(res_list)
+    res_df = pd.DataFrame(res_list)
     res_df.index = pd.to_datetime(res_df['time'], utc=False, errors='coerce')
     res_df = res_df.drop(['time'], axis=1)
 
