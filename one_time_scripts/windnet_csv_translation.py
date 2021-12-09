aea090d9b74d4f8b70650158e1735c2225a8384d
Jip Rietveld <jip_rietveld@hotmail.com>
2021-12-09 09:50:51 +0100

    Visualise trivial implementation

--- one_time_scripts/windnet_csv_translation.py
+++ one_time_scripts/windnet_csv_translation.py
@@ -1,6 +1,8 @@
 from csv import reader, writer
 import datetime as dt
 import dateutil.tz
+import pandas as pd
+import matplotlib.pyplot as plt
 
 ams = dateutil.tz.gettz('Europe/Amsterdam')
 utc = dateutil.tz.tzutc()
@@ -14,11 +16,11 @@ def trivial_kw_per_minute(total_kwh, number_of_minutes=5):
 
 
 def make_trivial_windnet_csv():
-    with open('../data/trivial_cleaned_windnet_data_sep_2020_sep_2021.csv', 'w+', newline='') as new_file:
+    with open('../data/windnet/trivial_cleaned_windnet_data_sep_2020_sep_2021.csv', 'w+', newline='') as new_file:
         csv_writer = writer(new_file)
         csv_writer.writerow(['time', 'neushoorntocht_consumed_kw', 'neushoorntocht_produced_kw',
                              'mammoettocht_consumed_kw', 'mammoettocht_produced_kw'])
-        with open('../data/base_windnet_data_sep_2020_sep_2021.csv', 'r') as read_obj:
+        with open('../data/windnet/base_windnet_data_sep_2020_sep_2021.csv', 'r') as read_obj:
             csv_reader = reader(read_obj)
             header = False
             for wind_net_data in csv_reader:
@@ -43,5 +45,53 @@ def make_trivial_windnet_csv():
                     time_tracker += dt.timedelta(minutes=1)
 
 
+def make_smarter_windnet_csv():
+    with open('../data/windnet/base_windnet_data_sep_2020_sep_2021.csv', 'r') as read_obj:
+        csv_reader = reader(read_obj)
+
+        res_list = []
+        header = False
+        counter = 0
+        for wind_net_data in csv_reader:
+            if not header:
+                header = True
+                continue
+            end_of_5m_interval = dt.datetime.strptime(wind_net_data[0], '%d/%m/%Y %H:%M').replace(tzinfo=ams)
+            end_of_5m_interval = end_of_5m_interval.astimezone(utc)
+            start_of_5m_interval = end_of_5m_interval - dt.timedelta(minutes=5)
+
+            time_tracker = start_of_5m_interval
+            neushoorntocht_consumed_kw = trivial_kw_per_minute(float(wind_net_data[1]))
+            neushoorntocht_produced_kw = trivial_kw_per_minute(float(wind_net_data[2]))
+            mammoettocht_consumed_kw = trivial_kw_per_minute(float(wind_net_data[3]))
+            mammoettocht_produced_kw = trivial_kw_per_minute(float(wind_net_data[4]))
+
+            while time_tracker < end_of_5m_interval:
+                time_tracker_str = time_tracker.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+                new_row = {
+                    'time': time_tracker_str,
+                    'neushoorntocht_consumed_kw': neushoorntocht_consumed_kw,
+                    'neushoorntocht_produced_kw': neushoorntocht_produced_kw,
+                    'mammoettocht_consumed_kw': mammoettocht_consumed_kw,
+                    'mammoettocht_produced_kw': mammoettocht_produced_kw
+                }
+                res_list.append(new_row)
+                if time_tracker.hour == 20 and time_tracker.minute == 20:
+                    print(new_row)
+                counter += 1
+                time_tracker += dt.timedelta(minutes=1)
+            if counter >= 1440:
+                break
+    res_df = pd.DataFrame.from_dict(res_list)
+    res_df.index = pd.to_datetime(res_df['time'], utc=False, errors='coerce')
+    res_df = res_df.drop(['time'], axis=1)
+
+    plt.plot(res_df.index, res_df['neushoorntocht_produced_kw'])
+    plt.title('Trivially extrapolated 5m wind data')
+    plt.xlabel('Time')
+    plt.ylabel('Produced power (kW)')
+    plt.show()
+
+
 if __name__ == '__main__':
-    make_trivial_windnet_csv()
+    make_smarter_windnet_csv()
