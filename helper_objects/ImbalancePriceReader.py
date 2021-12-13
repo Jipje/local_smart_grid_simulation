e7a3f52fe4f438b49b40cf7f04530a752f0f771b
Jip Rietveld <jip_rietveld@hotmail.com>
2021-12-13 14:24:41 +0100

    Add get specific time method

--- helper_objects/ImbalancePriceReader.py
+++ helper_objects/ImbalancePriceReader.py
@@ -24,10 +24,20 @@ class ImbalancePriceReader(object):
         start_of_day = day
         end_of_day = day + dt.timedelta(days=1)
 
-        filtered_df = self.market_df[(self.market_df.index > start_of_day) & (self.market_df.index <= end_of_day)]
+        return self.get_specific_time(start_of_day, end_of_day)
+
+    def get_specific_time(self, start_of_time=None, end_of_time=None):
+        if start_of_time is None:
+            start_of_time = dt.datetime(2020, 12, 27, tzinfo=utc)
+
+        if end_of_time is None:
+            end_of_time = start_of_time + dt.timedelta(days=1)
+
+        filtered_df = self.market_df[(self.market_df.index > start_of_time) & (self.market_df.index <= end_of_time)]
         return filtered_df
 
 
 if __name__ == '__main__':
     imbalance_price_reader = ImbalancePriceReader()
     print(imbalance_price_reader.get_day())
+    print(imbalance_price_reader.get_specific_time())
