9b314b3e66157d70532bba100a85513b01fbf969
Jip Rietveld <jip_rietveld@hotmail.com>
2021-12-15 17:23:32 +0100

    Huge commit waarin ik alles omgooi

--- one_time_scripts/helper_objects/SmartWindnetInterpolater.py
+++ one_time_scripts/helper_objects/SmartWindnetInterpolater.py
@@ -6,24 +6,56 @@ class SmartWindnetInterpolater(object):
     def trivial_kw_per_minute(self, total_kwh):
         return round(total_kwh / self.number_of_minutes * 60, 2)
 
-    def kw_per_minute(self, total_kwh, previous_kwh=None, next_kwh=None):
-        if previous_kwh is None:
-            previous_average = self.trivial_kw_per_minute(total_kwh)
+    def kw_per_minute(self, current_kw, previous_kw=None, next_kw=None):
+        average_diff = abs(current_kw - previous_kw) + abs(current_kw - next_kw) / 2
+        smallest_diff = min(current_kw, average_diff)
+
+        slope_factor = 0.3
+        parabole_factor = 0.2
+
+        if previous_kw > current_kw and next_kw > current_kw:
+            # current_kw is a dip
+            first_kw = current_kw + (smallest_diff * 0.3 * parabole_factor)
+            second_kw = current_kw + (smallest_diff * 0.2 * parabole_factor)
+
+            third_kw = current_kw + (smallest_diff * 0.2 * parabole_factor)
+            fourth_kw = current_kw + (smallest_diff * 0.3 * parabole_factor)
+
+            current_kw = current_kw - (smallest_diff * parabole_factor)
+
+        elif previous_kw < current_kw and next_kw < current_kw:
+            # current_kw is a peak
+            first_kw = current_kw - (smallest_diff * 0.3 * parabole_factor)
+            second_kw = current_kw - (smallest_diff * 0.2 * parabole_factor)
+
+            third_kw = current_kw - (smallest_diff * 0.2 * parabole_factor)
+            fourth_kw = current_kw - (smallest_diff * 0.3 * parabole_factor)
+
+            current_kw = current_kw + (smallest_diff * parabole_factor)
+
+        elif previous_kw <= current_kw and next_kw > current_kw:
+            # current_kw will be increasing
+            first_kw = current_kw - (smallest_diff * slope_factor)
+            second_kw = current_kw - (smallest_diff * 0.5 * slope_factor)
+            third_kw = current_kw + (smallest_diff * 0.5 * slope_factor)
+            fourth_kw = current_kw + (smallest_diff * slope_factor)
+        elif previous_kw > current_kw and next_kw <= current_kw:
+            # current_kw will be decreasing
+            first_kw = current_kw + (smallest_diff * slope_factor)
+            second_kw = current_kw + (smallest_diff * 0.5 * slope_factor)
+            third_kw = current_kw - (smallest_diff * 0.5 * slope_factor)
+            fourth_kw = current_kw - (smallest_diff * slope_factor)
         else:
-            previous_average = self.trivial_kw_per_minute(previous_kwh)
+            first_kw = current_kw
+            second_kw = current_kw
+            third_kw = current_kw
+            fourth_kw = current_kw
 
-        if next_kwh is None:
-            next_average = self.trivial_kw_per_minute(total_kwh)
-        else:
-            next_average = self.trivial_kw_per_minute(next_kwh)
-
-        current_average = self.trivial_kw_per_minute(total_kwh)
-
-        smallest_diff = min(abs(current_average - previous_average), (current_average - next_average))
-        diff_per_step = smallest_diff / 6
-
-        res = []
-        for i in range(self.number_of_minutes):
-            difference_this_step = (2 - i) * diff_per_step
-            res.append(current_average + difference_this_step)
+        res = [first_kw, second_kw, current_kw, third_kw, fourth_kw]
         return res
+
+
+if __name__ == '__main__':
+    smart_windnet_interpolater = SmartWindnetInterpolater()
+    print(smart_windnet_interpolater.kw_per_minute(10, 20, 30))
+    print(smart_windnet_interpolater.kw_per_minute(20, 10, 30))
---
+++ one_time_scripts/windnet_interpolation.py
@@ -0,0 +1,115 @@
+import datetime as dt
+import dateutil.tz
+import pandas as pd
+import matplotlib.pyplot as plt
+from one_time_scripts.helper_objects.SmartWindnetInterpolater import SmartWindnetInterpolater
+
+ams = dateutil.tz.gettz('Europe/Amsterdam')
+utc = dateutil.tz.tzutc()
+
+start_graph = dt.datetime(2021, 3, 10, 8, tzinfo=utc)
+end_graph = start_graph + dt.timedelta(minutes=1000)
+
+
+def trivial_interpolation_windnet():
+    base_windnet_df = pd.read_csv('../data/windnet/cleaned_windnet_data_aug_2020_sep_2021.csv')
+    base_windnet_df.index = pd.to_datetime(base_windnet_df['date'], utc=True, errors='coerce')
+    base_windnet_df.index = base_windnet_df.index - dt.timedelta(minutes=5)
+    base_windnet_df = base_windnet_df.drop(['date'], axis=1)
+
+    base_windnet_df['nht_usage_kwh'] = base_windnet_df['nht_usage_kwh'] / 5
+    base_windnet_df['nht_production_kwh'] = base_windnet_df['nht_production_kwh'] / 5
+    base_windnet_df['mmt_usage_kwh'] = base_windnet_df['mmt_usage_kwh'] / 5
+    base_windnet_df['mmt_production_kwh'] = base_windnet_df['mmt_production_kwh'] / 5
+
+    base_windnet_df = base_windnet_df.resample('1T').pad()
+    return base_windnet_df
+
+
+def pandas_linear_interpolation_windnet():
+    base_windnet_df = pd.read_csv('../data/windnet/cleaned_windnet_data_aug_2020_sep_2021.csv')
+    base_windnet_df.index = pd.to_datetime(base_windnet_df['date'], utc=True, errors='coerce')
+    base_windnet_df.index = base_windnet_df.index - dt.timedelta(minutes=5)
+    base_windnet_df = base_windnet_df.drop(['date', 'nht_usage_kwh', 'nht_production_kwh', 'mmt_usage_kwh', 'mmt_production_kwh'], axis=1)
+
+    base_windnet_df = base_windnet_df.resample('1T').interpolate()
+
+    base_windnet_df['nht_usage_kwh'] = base_windnet_df['nht_usage_kw'] / 60
+    base_windnet_df['nht_production_kwh'] = base_windnet_df['nht_production_kw'] / 60
+    base_windnet_df['mmt_usage_kwh'] = base_windnet_df['mmt_usage_kw'] / 60
+    base_windnet_df['mmt_production_kwh'] = base_windnet_df['mmt_production_kw'] / 60
+
+    return base_windnet_df
+
+    # filtered_df = base_windnet_df.loc[start_graph:end_graph]
+    #
+    # plt.plot(filtered_df.index, filtered_df['nht_production_kw'])
+    # plt.title('Pandas linear interpolation 5m wind data')
+    # plt.xlabel('Time')
+    # plt.ylabel('Produced power (kW)')
+    # plt.show()
+
+
+def smarter_interpolation_windnet():
+    smarter_interpolater = SmartWindnetInterpolater()
+
+    base_windnet_df = pd.read_csv('../data/windnet/cleaned_windnet_data_aug_2020_sep_2021.csv')
+    base_windnet_df = base_windnet_df.drop(['nht_usage_kwh', 'nht_production_kwh', 'mmt_usage_kwh', 'mmt_production_kwh'], axis=1)
+
+    df2 = pd.DataFrame([['23/02/1997 20:20', None, None, None, None], ['23/02/1997 20:22', None, None, None, None]],
+                       columns=['date', 'nht_usage_kw', 'nht_production_kw', 'mmt_usage_kw', 'mmt_production_kw'],
+                       index=[-1, len(base_windnet_df)])
+    shuffled_kw_windnet = base_windnet_df.append(df2)
+
+    res_list = []
+
+    for index, current_wind_net_data in shuffled_kw_windnet.iterrows():
+        if index == -1 or index == len(base_windnet_df):
+            continue
+        previous_wind_net_data = shuffled_kw_windnet.iloc[index - 1]
+        next_wind_net_data = shuffled_kw_windnet.iloc[index + 1]
+
+        end_of_5m_interval = dt.datetime.strptime(current_wind_net_data[0], '%Y-%m-%d %H:%M:%S%z')
+        start_of_5m_interval = end_of_5m_interval - dt.timedelta(minutes=5)
+
+        neushoorntocht_consumed_kw = smarter_interpolater.kw_per_minute(float(current_wind_net_data[1]),
+                                                                        float(previous_wind_net_data[1]),
+                                                                        float(next_wind_net_data[1]))
+        neushoorntocht_produced_kw = smarter_interpolater.kw_per_minute(float(current_wind_net_data[2]),
+                                                                        float(previous_wind_net_data[2]),
+                                                                        float(next_wind_net_data[2]))
+        mammoettocht_consumed_kw = smarter_interpolater.kw_per_minute(float(current_wind_net_data[3]),
+                                                                      float(previous_wind_net_data[3]),
+                                                                      float(next_wind_net_data[3]))
+        mammoettocht_produced_kw = smarter_interpolater.kw_per_minute(float(current_wind_net_data[4]),
+                                                                      float(previous_wind_net_data[4]),
+                                                                      float(next_wind_net_data[4]))
+
+        for i in range(5):
+            time_tracker = start_of_5m_interval + dt.timedelta(minutes=i)
+            time_tracker_str = time_tracker.strftime('%Y-%m-%dT%H:%M:%S%z')
+            new_row = {
+                'time': time_tracker_str,
+                'nht_usage_kw': neushoorntocht_consumed_kw[i],
+                'nht_production_kw': neushoorntocht_produced_kw[i],
+                'mmt_usage_kw': mammoettocht_consumed_kw[i],
+                'mmt_production_kw': mammoettocht_produced_kw[i]
+            }
+            res_list.append(new_row)
+
+    res_df = pd.DataFrame(res_list)
+    res_df.index = pd.to_datetime(res_df['time'], utc=True, errors='coerce')
+    res_df = res_df.drop(['time'], axis=1)
+
+    res_df['nht_usage_kwh'] = res_df['nht_usage_kw'] / 60
+    res_df['nht_production_kwh'] = res_df['nht_production_kw'] / 60
+    res_df['mmt_usage_kwh'] = res_df['mmt_usage_kw'] / 60
+    res_df['mmt_production_kwh'] = res_df['mmt_production_kw'] / 60
+
+    return res_df
+
+
+if __name__ == '__main__':
+    print(trivial_interpolation_windnet())
+    print(pandas_linear_interpolation_windnet())
+    print(smarter_interpolation_windnet())
