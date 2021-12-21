38692ebf4552ff92c41833c7aafa86d251413ee8
Jip Rietveld <jip_rietveld@hotmail.com>
2021-12-21 16:47:00 +0100

    Add some errors to print

--- one_time_scripts/solar_scripts/solar_merger.py
+++ one_time_scripts/solar_scripts/solar_merger.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import datetime as dt
 import dateutil.tz
+from sklearn.metrics import mean_squared_error
 
 ams = dateutil.tz.gettz('Europe/Amsterdam')
 utc = dateutil.tz.tzutc()
@@ -27,13 +28,48 @@ if __name__ == '__main__':
     radiation_df = pd.read_csv('../../data/solar_data/radiation_with_forecast/cleaned_radiation_forecast_and_values.csv', parse_dates=[0], date_parser=date_parser)
     radiation_df.index = pd.to_datetime(radiation_df['time'], errors='coerce', utc=True)
     radiation_df = radiation_df.drop('time', axis=1)
-    radiation_df = radiation_df.resample('15T').pad()
     # print(radiation_df)
 
+    # radiation_df = radiation_df.resample('15T').pad()
     # res_df = solar_power_mw_df.merge(solar_forecast_df, how='inner', left_index=True, right_index=True)
     # res_df = res_df.merge(radiation_df, how='inner', left_index=True, right_index=True)
     # print(res_df)
 
+    radiation_df['APE_d_1'] = abs((radiation_df['radiation'] - radiation_df['radiation_d_1']) / radiation_df['radiation'])
+    radiation_df['APE_d_3'] = abs((radiation_df['radiation'] - radiation_df['radiation_d_3']) / radiation_df['radiation'])
+    radiation_df['APE_d_5'] = abs((radiation_df['radiation'] - radiation_df['radiation_d_5']) / radiation_df['radiation'])
+    # print(radiation_df)
+    print('MAPE d-1')
+    print(radiation_df['APE_d_1'].mean())
+    print('MAPE d-3')
+    print(radiation_df['APE_d_3'].mean())
+    print('MAPE d-5')
+    print(radiation_df['APE_d_5'].mean())
+
+    print('-----------------------------------')
+
+    radiation_df['sAPE_d_1'] = abs((radiation_df['radiation_d_1'] - radiation_df['radiation'])) / (radiation_df['radiation'] + radiation_df['radiation_d_1']) / 2
+    radiation_df['sAPE_d_3'] = abs((radiation_df['radiation_d_3'] - radiation_df['radiation'])) / (radiation_df['radiation'] + radiation_df['radiation_d_3']) / 2
+    radiation_df['sAPE_d_5'] = abs((radiation_df['radiation_d_5'] - radiation_df['radiation'])) / (radiation_df['radiation'] + radiation_df['radiation_d_5']) / 2
+    # print(radiation_df)
+    print('sMAPE d-1')
+    print(radiation_df['sAPE_d_1'].mean())
+    print('sMAPE d-3')
+    print(radiation_df['sAPE_d_3'].mean())
+    print('sMAPE d-5')
+    print(radiation_df['sAPE_d_5'].mean())
+
+    print('-----------------------------------')
+
+    # print(radiation_df)
+    print('RMSE d-1')
+    print(mean_squared_error(radiation_df['radiation'], radiation_df['radiation_d_1'], squared=False))
+    print('RMSE d-3')
+    print(mean_squared_error(radiation_df['radiation'], radiation_df['radiation_d_3'], squared=False))
+    print('RMSE d-5')
+    print(mean_squared_error(radiation_df['radiation'], radiation_df['radiation_d_5'], squared=False))
+
+    radiation_df = radiation_df.resample('15T').pad()
     start_of_set = dt.datetime(2021, 8, 10, tzinfo=utc)
     end_of_set = dt.datetime(2021, 8, 12, tzinfo=utc)
     radiation_df = radiation_df[start_of_set:end_of_set]
