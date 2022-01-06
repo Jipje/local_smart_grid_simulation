4710a2015a1fc4a5b0f7a3cc83689beb8c6b1da9
Jip Rietveld <jip_rietveld@hotmail.com>
2022-01-06 16:52:48 +0100

    Made the visualization clearer

--- one_time_scripts/visualisations/strategy_visualisation.py
+++ one_time_scripts/visualisations/strategy_visualisation.py
@@ -1,13 +1,14 @@
 import os
+
+from helper_objects.PointBasedStrategy import PointBasedStrategy
 from helper_objects.StrategyBattery import StrategyBattery
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
+from helper_objects.Strategy import Strategy
 
-if __name__ == '__main__':
-    strategy_one_path = '..{0}..{0}data{0}strategies{0}cleaner_simplified_passive_imbalance_1.csv'.format(os.path.sep)
-    strategy_one = StrategyBattery(name='TESTING', strategy_csv=strategy_one_path, price_step_size=1)
-    print(strategy_one)
-    strategy_matrix = strategy_one.strategy_matrix
+
+def visualize_strategy(strategy: Strategy):
+    strategy_matrix = strategy.strategy_matrix
     red = (0.934, 0.277, 0.434)
     blue = (0.129, 0.535, 0.492)
     white = (1, 1, 1)
@@ -26,7 +27,7 @@ if __name__ == '__main__':
             strategy_matrix[row_index][column_index] = new_item
 
     strategy_matrix = [*zip(*strategy_matrix)]
-    plt.title('Strategy One')
+    plt.title('{}'.format(strategy.name))
     plt.ylabel('Imbalance price')
     plt.xlabel('State of charge (SoC %)')
 
@@ -37,3 +38,22 @@ if __name__ == '__main__':
     plt.imshow(strategy_matrix, interpolation='none', origin='lower')
 
     plt.show()
+
+if __name__ == '__main__':
+    csv_strategy_path = '..{0}..{0}data{0}strategies{0}cleaner_simplified_passive_imbalance_1.csv'.format(os.path.sep)
+    csv_strategy = StrategyBattery(name='CSV Strategy', strategy_csv=csv_strategy_path, price_step_size=1)
+    visualize_strategy(csv_strategy)
+
+    point_based_strat = PointBasedStrategy('Point Based Strategy', price_step_size=1)
+
+    point_based_strat.add_point((50, 50, 'CHARGE'))
+    point_based_strat.add_point((70, 30, 'CHARGE'))
+    point_based_strat.add_point((95, 0, 'CHARGE'))
+
+    point_based_strat.add_point((40, 100, 'DISCHARGE'))
+    point_based_strat.add_point((70, 80, 'DISCHARGE'))
+    point_based_strat.add_point((95, 65, 'DISCHARGE'))
+
+    point_based_strat.upload_strategy()
+
+    visualize_strategy(point_based_strat)
