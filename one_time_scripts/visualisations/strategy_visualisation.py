c6e6e9be9ed858f415f2b60bbbeed316c55c6d41
Jip Rietveld <jip_rietveld@hotmail.com>
2022-01-06 12:51:39 +0100

    Created a graph of the data

--- one_time_scripts/visualisations/strategy_visualisation.py
+++ one_time_scripts/visualisations/strategy_visualisation.py
@@ -1,7 +1,29 @@
 import os
 from helper_objects.StrategyBattery import StrategyBattery
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     strategy_one_path = '..{0}..{0}data{0}strategies{0}cleaner_simplified_passive_imbalance_1.csv'.format(os.path.sep)
     strategy_one = StrategyBattery(strategy_csv=strategy_one_path)
     print(strategy_one)
+    strategy_matrix = strategy_one.strategy_matrix
+    red = 1
+    blue = 0.5
+    white = 0
+
+    for row_index in range(len(strategy_matrix)):
+        row = strategy_matrix[row_index]
+        for column_index in range(len(row)):
+            item = row[column_index]
+
+            new_item = white
+            if item == 'CHARGE':
+                new_item = red
+            elif item == 'DISCHARGE':
+                new_item = blue
+
+            strategy_matrix[row_index][column_index] = new_item
+
+    plt.imshow(strategy_matrix, interpolation='none')
+
+    plt.show()
