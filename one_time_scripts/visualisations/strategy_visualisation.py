04f53f605e9b52ff93a42ce6230174a541a53f0e
Jip Rietveld <jip_rietveld@hotmail.com>
2022-01-06 13:22:10 +0100

    Added a legend

--- one_time_scripts/visualisations/strategy_visualisation.py
+++ one_time_scripts/visualisations/strategy_visualisation.py
@@ -1,15 +1,16 @@
 import os
 from helper_objects.StrategyBattery import StrategyBattery
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 
 if __name__ == '__main__':
     strategy_one_path = '..{0}..{0}data{0}strategies{0}cleaner_simplified_passive_imbalance_1.csv'.format(os.path.sep)
     strategy_one = StrategyBattery(strategy_csv=strategy_one_path, price_step_size=1)
     print(strategy_one)
     strategy_matrix = strategy_one.strategy_matrix
-    red = (239, 71, 111)
-    blue = (33, 137, 126)
-    white = (250, 250, 250)
+    red = (0.934, 0.277, 0.434)
+    blue = (0.129, 0.535, 0.492)
+    white = (1, 1, 1)
 
     for row_index in range(len(strategy_matrix)):
         row = strategy_matrix[row_index]
@@ -28,6 +29,11 @@ if __name__ == '__main__':
     plt.title('Strategy One')
     plt.ylabel('Imbalance price')
     plt.xlabel('State of charge (SoC %)')
+
+    legend_elements = [Patch(facecolor=red, edgecolor='black', label='CHARGE'),
+                       Patch(facecolor=white, edgecolor='black', label='WAIT'),
+                       Patch(facecolor=blue, edgecolor='black', label='DISCHARGE')]
+    plt.legend(handles=legend_elements, loc='lower left')
     plt.imshow(strategy_matrix, interpolation='none', origin='lower')
 
     plt.show()
