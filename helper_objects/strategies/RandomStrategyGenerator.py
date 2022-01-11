e04e83dbbd136ebec98ee0d549321d01acd35964
Jip Rietveld <jip_rietveld@hotmail.com>
2022-01-11 08:40:12 +0100

    Make the strategy fully random

--- helper_objects/strategies/RandomStrategyGenerator.py
+++ helper_objects/strategies/RandomStrategyGenerator.py
@@ -3,7 +3,7 @@ from helper_objects.strategies.PointBasedStrategy import PointBasedStrategy
 from one_time_scripts.visualisations.strategy_visualisation import visualize_strategy
 
 
-def generate_random_strategy(seed=None, name=None):
+def generate_fully_random_strategy(seed=None, name=None):
     if seed is not None:
         random.seed(seed)
         if name is None:
@@ -16,29 +16,13 @@ def generate_random_strategy(seed=None, name=None):
     point_based_strat = PointBasedStrategy(name, price_step_size=price_step_size)
     number_of_points = random.randint(1, 5)
 
-    charge_soc = None
-    charge_price = None
-    discharge_soc = None
-    discharge_price = None
     for _ in range(number_of_points):
-        if charge_soc is None and charge_price is None:
-            state_of_charge_perc = random.randint(6, 20)
-            imbalance_price = random.randrange(0, 100, 5)
-        else:
-            state_of_charge_perc = random.randint(charge_soc + 1, 95)
-            imbalance_price = random.randrange(-100, charge_price - 1, 5)
-        charge_soc = state_of_charge_perc
-        charge_price = imbalance_price
+        state_of_charge_perc = random.randint(6, 95)
+        imbalance_price = random.randrange(-100, 200, 5)
         point_based_strat.add_point((state_of_charge_perc, imbalance_price, 'CHARGE'))
 
-        if discharge_soc is None and discharge_price is None:
-            state_of_charge_perc = random.randint(6, 20)
-            imbalance_price = random.randrange(100, 200, 5)
-        else:
-            state_of_charge_perc = random.randint(discharge_soc + 1, 95)
-            imbalance_price = random.randrange(-100, discharge_price - 1, 5)
-        discharge_soc = state_of_charge_perc
-        discharge_price = imbalance_price
+        state_of_charge_perc = random.randint(6, 95)
+        imbalance_price = random.randrange(-100, 200, 5)
         point_based_strat.add_point((state_of_charge_perc, imbalance_price, 'DISCHARGE'))
 
     point_based_strat.upload_strategy()
@@ -46,5 +30,5 @@ def generate_random_strategy(seed=None, name=None):
 
 
 if __name__ == '__main__':
-    random_strategy = generate_random_strategy()
+    random_strategy = generate_fully_random_strategy()
     visualize_strategy(random_strategy)
