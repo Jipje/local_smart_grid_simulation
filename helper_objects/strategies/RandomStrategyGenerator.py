57ae0b370238c97f13aa8d4fdf09e9949b17ac52
Jip Rietveld <jip_rietveld@hotmail.com>
2022-01-11 10:58:31 +0100

    Handle the strategy price jumps

--- helper_objects/strategies/RandomStrategyGenerator.py
+++ helper_objects/strategies/RandomStrategyGenerator.py
@@ -37,32 +37,32 @@ def generate_random_discharge_relative_strategy(seed=None, name=None):
     if name is None:
         name = 'Randomly generated strategy. Seed={}'.format(seed)
 
-    price_step_size = 5
-    point_based_strat = PointBasedStrategy(name, price_step_size=price_step_size)
+    strategy_price_step_size = 1
+    point_based_strat = PointBasedStrategy(name, price_step_size=strategy_price_step_size)
     number_of_points = random.randint(2, 5)
 
     soc_step_size = int(89/number_of_points)
     price_step_size = int(300/number_of_points)
-    if price_step_size % 5 != 0:
-        price_step_size = price_step_size - (price_step_size % 5)
-    charge_soc = 5
+    if price_step_size % strategy_price_step_size != 0:
+        price_step_size = price_step_size - (price_step_size % strategy_price_step_size)
+    charge_soc = 1
     charge_price = 201
     discharge_soc = 0
     discharge_price = 201
     for i in range(1, number_of_points + 1):
         charge_soc = random.randint(charge_soc + 1, i * soc_step_size)
-        charge_price = random.randrange(200 - i * price_step_size, charge_price - 1, 5)
+        charge_price = random.randrange(200 - i * price_step_size, charge_price - 1, strategy_price_step_size)
         point_based_strat.add_point((charge_soc, charge_price, 'CHARGE'))
 
         discharge_soc = min(max(charge_soc, discharge_soc) + random.randint(5, soc_step_size), 94)
-        discharge_price = random.randrange(charge_price, discharge_price - 1, 5)
+        discharge_price = random.randrange(charge_price, discharge_price - 1, strategy_price_step_size)
         point_based_strat.add_point((discharge_soc, discharge_price, 'DISCHARGE'))
 
     charge_soc = 95
-    charge_price = random.randrange(-100, charge_price - 1, 5)
+    charge_price = random.randrange(-100, charge_price - 1, strategy_price_step_size)
     point_based_strat.add_point((charge_soc, charge_price, 'CHARGE'))
     discharge_soc = 95
-    discharge_price = random.randrange(charge_price, discharge_price - 1, 5)
+    discharge_price = random.randrange(charge_price, discharge_price - 1, strategy_price_step_size)
     point_based_strat.add_point((discharge_soc, discharge_price, 'DISCHARGE'))
 
     point_based_strat.upload_strategy()
