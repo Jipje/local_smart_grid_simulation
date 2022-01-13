fc6ec38fff4af0b2f9f5224ca8f4b375f2c1ab6b
Jip Rietveld <jip_rietveld@hotmail.com>
2022-01-13 10:18:26 +0100

    Fix limited capacity tests

--- tests/test_battery_limited_charge_or_discharge_capacity.py
+++ tests/test_battery_limited_charge_or_discharge_capacity.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 
+from helper_objects.strategies.CsvStrategy import CsvStrategy
 from network_objects.Battery import Battery
 from network_objects.decorators.LimitedChargeOrDischargeCapacity import LimitedChargeOrDischargeCapacity
 
@@ -11,17 +12,19 @@ class TestLimitedChargeOrDischargeCapacity(unittest.TestCase):
     def setUpClass(cls) -> None:
         strategy_one_path = '..{0}data{0}strategies{0}cleaner_simplified_passive_imbalance_1.csv'.format(os.path.sep)
         try:
-            Battery('LimitedChargeOrDischargeTest', 7500, 12000, battery_strategy_csv=strategy_one_path, starting_soc_kwh=3750)
+            CsvStrategy('Rhino strategy 1', strategy_csv=strategy_one_path)
         except FileNotFoundError:
             strategy_one_path = 'data{0}strategies{0}cleaner_simplified_passive_imbalance_1.csv'.format(os.path.sep)
         cls.strategy_one_path = strategy_one_path
 
     def test_initialisation(self):
-        rhino = Battery('LimitedChargeOrDischargeTest', 7500, 12000, battery_strategy_csv=self.strategy_one_path, starting_soc_kwh=0)
+        csv_strategy = CsvStrategy('Rhino strategy 1', strategy_csv=self.strategy_one_path)
+        rhino = Battery('test_simple_congestion', 7500, 12000, strategy=csv_strategy, starting_soc_kwh=0)
         LimitedChargeOrDischargeCapacity(rhino, 5, 6)
 
     def test_max_charge(self):
-        rhino = Battery('LimitedChargeOrDischargeTest', 7500, 12000, battery_strategy_csv=self.strategy_one_path, starting_soc_kwh=6825)
+        csv_strategy = CsvStrategy('Rhino strategy 1', strategy_csv=self.strategy_one_path)
+        rhino = Battery('test_max_charge', 7500, 12000, strategy=csv_strategy, starting_soc_kwh=6825)
         LimitedChargeOrDischargeCapacity(rhino, 2, 3)
         # Normal charging
         rhino.take_step([-200, -200, 500, -500], [0, 1])
@@ -34,7 +37,8 @@ class TestLimitedChargeOrDischargeCapacity(unittest.TestCase):
         self.assertEqual(7124, rhino.state_of_charge_kwh)
 
     def test_max_discharge(self):
-        rhino = Battery('LimitedChargeOrDischargeTest', 7500, 12000, battery_strategy_csv=self.strategy_one_path, starting_soc_kwh=700)
+        csv_strategy = CsvStrategy('Rhino strategy 1', strategy_csv=self.strategy_one_path)
+        rhino = Battery('test_max_discharge', 7500, 12000, strategy=csv_strategy, starting_soc_kwh=700)
         LimitedChargeOrDischargeCapacity(rhino, 2, 3)
         # Normal discharging
         rhino.take_step([500, 500, 500, -500], [0, 1])
