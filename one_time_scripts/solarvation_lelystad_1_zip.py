9c8a85ac752583a764bc380c2b84b15ce519b68c
Jip Rietveld <jip_rietveld@hotmail.com>
2022-02-22 09:09:32 +0100

    Zipped the tennet balans delta to lelystad_1

--- one_time_scripts/solarvation_lelystad_1_zip.py
+++ one_time_scripts/solarvation_lelystad_1_zip.py
@@ -11,3 +11,16 @@ if __name__ == '__main__':
     solar_df = solar_df.drop(['time_ams', 'time_utc'], axis=1)
     solar_df = solar_df.resample('1T').interpolate(method='linear', limit=4)
     print(solar_df)
+
+    balans_delta_df = pd.read_csv('../data/tennet_balans_delta/tennet_balans_delta_2021.csv')
+    balans_delta_df.index = pd.to_datetime(balans_delta_df['time'], format='%Y-%m-%dT%H:%M:%S.000Z', utc=True, errors='coerce')
+    balans_delta_df = balans_delta_df.drop(['time'], axis=1)
+    print(balans_delta_df)
+
+    res_df = pd.merge(balans_delta_df, solar_df, left_index=True, right_index=True)
+    res_df['power'] = res_df['power'].astype('float64')
+    res_df['irradiance'] = res_df['irradiance'].astype('float64')
+    res_df['expected_power'] = res_df['expected_power'].astype('float64')
+    res_df['losses'] = res_df['losses'].astype('float64')
+    # res_df.to_csv('../data/environments/lelystad_1_2021.csv')
+    print(res_df)
