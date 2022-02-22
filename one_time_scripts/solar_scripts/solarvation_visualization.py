57ad6570d1cceaa1ed89b552d3f91fc628943b5c
Jip Rietveld <jip_rietveld@hotmail.com>
2022-02-22 10:09:40 +0100

    Add monthly plots

--- one_time_scripts/solar_scripts/solarvation_visualization.py
+++ one_time_scripts/solar_scripts/solarvation_visualization.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     solarvation_df = pd.read_csv(solarvation_filename, parse_dates=[0], date_parser=date_parser)
     solarvation_df.index = pd.to_datetime(solarvation_df['time_utc'], errors='coerce', utc=True)
     solarvation_df = solarvation_df.drop('time_utc', axis=1)
-    print(solarvation_df)
+    # print(solarvation_df)
 
     # ['tennet_balansdelta.mean_max_price', 'tennet_balansdelta.mean_mid_price', 'tennet_balansdelta.mean_min_price',
     #  'power', 'irradiance', 'expected_power', 'lower_range', 'upper_range', 'losses']
@@ -39,4 +39,39 @@ if __name__ == '__main__':
     plt.ylabel('Generated power 1m (kW)')
     plt.xlabel('Hour in which power was generated (UTC)')
     plt.title('Scatterplot of power generation by solar field Lelystad 1')
+    plt.ylim(0, 20000)
+    plt.show()
+
+    fig, axs = plt.subplots(4, 3, figsize=(12, 9))
+    for i in range(1, 13):
+        month = i
+        start_of_month = dt.datetime(2021, month, 1, tzinfo=utc)
+        temp_day = start_of_month.replace(day=28) + dt.timedelta(days=4)
+        end_of_month = temp_day - dt.timedelta(days=temp_day.day)
+
+        if month in [1, 4, 7, 10]:
+            axes_y = 0
+        elif month in [2, 5, 8, 11]:
+            axes_y = 1
+        else:
+            axes_y = 2
+
+        axes_x = None
+        for j in range(1, 5):
+            if month <= (j * 3):
+                axes_x = j - 1
+                break
+
+        # print('{} - {}, {}'.format(start_of_month.strftime('%B'), axes_x, axes_y))
+
+        month_df = solarvation_df[start_of_month:end_of_month]
+        axs[axes_x, axes_y].scatter(month_df['hour_of_production'], month_df['power'])
+        fig.suptitle('Scatterplot of generated power per month')
+        # plt.ylabel('Generated power 1m (kW)')
+        # plt.xlabel('Hour in which power was generated (UTC)')
+        axs[axes_x, axes_y].set_title('{}'.format(start_of_month.strftime('%B')))
+        axs[axes_x, axes_y].set_ylim((0, 20000))
+    for ax in axs.flat:
+        ax.set(xlabel='Hour (UTC)', ylabel='Generated power (kW)')
+        ax.label_outer()
     plt.show()
