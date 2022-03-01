7728594f1b4b6c1978afa14e186bcc6973d8210c
Jip Rietveld <jip_rietveld@hotmail.com>
2022-03-01 14:09:13 +0100

    Introduced innax metre to the windfarm and corrected tests

--- tests/test_windfarm.py
+++ tests/test_windfarm.py
@@ -9,19 +9,19 @@ class TestWindFarm(unittest.TestCase):
         wind_farm = WindFarm('TEST', 10000)
         self.assertEqual(10000, wind_farm.max_kw)
         self.assertEqual(0, wind_farm.available_kw)
-        self.assertEqual(0, wind_farm.earnings)
+        self.assertEqual(0, wind_farm.earnings())
 
     def test_set_available_kw_this_action(self):
         wind_farm = WindFarm('TEST', 10000)
         self.assertEqual(10000, wind_farm.max_kw)
         self.assertEqual(0, wind_farm.available_kw)
-        self.assertEqual(0, wind_farm.earnings)
+        self.assertEqual(0, wind_farm.earnings())
 
         wind_farm.set_available_kw_this_action(7500)
 
         self.assertEqual(10000, wind_farm.max_kw)
         self.assertEqual(7500, wind_farm.available_kw)
-        self.assertEqual(0, wind_farm.earnings)
+        self.assertEqual(0, wind_farm.earnings())
 
     def test_take_step_mock(self):
         wind_farm = WindFarm('TEST', 10000)
@@ -51,10 +51,10 @@ class TestWindFarm(unittest.TestCase):
         action_parameters = [0, 1, 2]
         wind_farm.take_step(environment_step, action_parameters)
         self.assertEqual(-12000, wind_farm.available_kw)
-        self.assertEqual(-200, wind_farm.ptu_total_action)
+        self.assertEqual(-200, wind_farm.innax_metre.ptu_total_action)
 
     def test_take_imbalance_action(self):
         wind_farm = WindFarm('TEST', 10000)
         wind_farm.set_available_kw_this_action(-12000)
         wind_farm.take_imbalance_action(50, 50)
-        self.assertEqual(-200, wind_farm.ptu_total_action)
+        self.assertEqual(-200, wind_farm.innax_metre.ptu_total_action)
