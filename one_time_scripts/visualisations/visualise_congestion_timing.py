c3b41ed3e6748d3c597cab685e2be6afa4ec272c
Jip Rietveld <jip_rietveld@hotmail.com>
2022-03-16 12:40:56 +0100

    Adding opacity based on kwh

--- one_time_scripts/visualisations/visualise_congestion_timing.py
+++ one_time_scripts/visualisations/visualise_congestion_timing.py
@@ -17,18 +17,20 @@ if __name__ == '__main__':
 
     prep_starts = res_df.loc['prep_start'].array
     congestion_starts = res_df.loc['congestion_start'].array
-    preparing_max_kwh = res_df.loc['prep_max_soc']
+    preparing_max_kwh = res_df.loc['prep_max_soc'].array
     solving_congestion_until = res_df.loc['congestion_end'].array
 
     prep_starts_y_min = []
     prep_starts_y_max = []
     y_ticks = []
     colors = []
+    largest_prep_kwh = -1
     for i in range(len(prep_starts)):
         if prep_starts[i] is NaT:
             prep_starts[i] = dt.datetime(1970, 1, 1, 0, 0, tzinfo=utc)
             congestion_starts[i] = dt.datetime(1970, 1, 1, 0, 0, tzinfo=utc)
             solving_congestion_until[i] = dt.datetime(1970, 1, 1, 0, 0, tzinfo=utc)
+            preparing_max_kwh[i] = 0
             prep_starts_y_min.append(None)
             prep_starts_y_max.append(None)
         else:
@@ -37,6 +39,8 @@ if __name__ == '__main__':
             solving_congestion_until[i] = dt.datetime(1970, 1, 1, solving_congestion_until[i].hour, solving_congestion_until[i].minute, tzinfo=utc)
             prep_starts_y_min.append((i * 20) + 5)
             prep_starts_y_max.append((i * 20) + 20)
+            if preparing_max_kwh[i] > largest_prep_kwh:
+                largest_prep_kwh = preparing_max_kwh[i]
         colors.append('#%06X' % randint(0, 0xFFFFFF))
         y_ticks.append((i * 20) + 12.5)
 
@@ -44,10 +48,11 @@ if __name__ == '__main__':
         y_min = prep_starts_y_min[i]
         y_max = prep_starts_y_max[i]
         color = colors[i]
+        opacity = preparing_max_kwh[i] / largest_prep_kwh * 0.75 + 0.1
         plt.plot([prep_starts[i], prep_starts[i]], [y_min, y_max], color=color)
-        plt.fill([prep_starts[i], prep_starts[i], congestion_starts[i], congestion_starts[i]], [y_min, y_max, y_max, y_min], color=color, alpha=0.5)
+        plt.fill([prep_starts[i], prep_starts[i], congestion_starts[i], congestion_starts[i]], [y_min, y_max, y_max, y_min], color=color, alpha=opacity)
         plt.plot([congestion_starts[i], congestion_starts[i]], [y_min, y_max], color=color)
-        plt.fill([congestion_starts[i], congestion_starts[i], solving_congestion_until[i], solving_congestion_until[i]], [y_min, y_max, y_max, y_min], color=color, alpha=0.5)
+        plt.fill([congestion_starts[i], congestion_starts[i], solving_congestion_until[i], solving_congestion_until[i]], [y_min, y_max, y_max, y_min], color=color, alpha=opacity)
         plt.plot([solving_congestion_until[i], solving_congestion_until[i]], [y_min, y_max], color=color)
 
     my_fmt = mdates.DateFormatter('%H:%M')
