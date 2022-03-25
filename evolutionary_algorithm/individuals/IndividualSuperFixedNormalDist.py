d47f7bdffe78e2ad4e312b2d98ab925e59628a8c
Jip Rietveld <jip_rietveld@hotmail.com>
2022-03-25 11:17:44 +0100

    Initial work

--- evolutionary_algorithm/individuals/IndividualSuperFixedNormalDist.py
+++ evolutionary_algorithm/individuals/IndividualSuperFixedNormalDist.py
@@ -20,35 +20,47 @@ class IndividualSuperFixedNormalDist(StrategyIndividual):
         for i in range(len(original_charge)):
             original_point = original_charge[i]
             other_point = other_charge[i]
-            assert original_point[2] == other_point[2]
-            new_point = [None, None, None]
-            random_dist = random.random()
-            for j in range(2):
-                new_point[j] = int(min(original_point[j], other_point[j]) +
-                                   random_dist * abs(original_point[j] - other_point[j]))
-            new_point[0] = max(new_point[0], 5)
-            new_point[0] = min(new_point[0], 95)
-            new_point[1] = new_point[1] - new_point[1] % 5
-            new_point[2] = 'CHARGE'
-            new_individual.add_point((new_point[0], new_point[1], new_point[2]))
+            new_individual.add_point(self.create_new_point(original_point, other_point, pair_params))
 
             original_point = original_discharge[i]
             other_point = other_discharge[i]
-            assert original_point[2] == other_point[2]
-            new_point = [None, None, None]
-            for j in range(2):
-                new_point[j] = int(min(original_point[j], other_point[j]) +
-                                   random_dist * abs(original_point[j] - other_point[j]))
-            new_point[0] = max(new_point[0], 5)
-            new_point[0] = min(new_point[0], 95)
-            new_point[1] = new_point[1] + 5 - new_point[1] % 5
-            new_point[2] = 'DISCHARGE'
-            new_individual.add_point((new_point[0], new_point[1], new_point[2]))
+            new_individual.add_point(self.create_new_point(original_point, other_point, pair_params))
         new_individual.upload_strategy()
         return IndividualSuperFixedNormalDist(new_individual)
 
     def mutate(self, mutate_params):
         return self
+
+    def create_new_point(self, original_point, other_point, pair_params):
+        try:
+            max_soc_perc = pair_params['max_soc_perc']
+            min_soc_perc = pair_params['min_soc_perc']
+        except KeyError:
+            max_soc_perc = 95
+            min_soc_perc = 5
+
+        try:
+            price_step_size = pair_params['price_step_size']
+        except KeyError:
+            price_step_size = 5
+
+        assert original_point[2] == other_point[2]
+        new_point = [None, None, None]
+
+        new_point = self.generate_new_point(new_point, original_point, other_point)
+
+        new_point[0] = max(new_point[0], min_soc_perc)
+        new_point[0] = min(new_point[0], max_soc_perc)
+        new_point[1] = new_point[1] - new_point[1] % price_step_size
+        new_point[2] = original_point[2]
+        return new_point[0], new_point[1], new_point[2]
+
+    def generate_new_point(self, new_point, original_point, other_point):
+        random_dist = random.random()
+        for j in range(2):
+            new_point[j] = int(min(original_point[j], other_point[j]) +
+                               random_dist * abs(original_point[j] - other_point[j]))
+        return new_point
 
 
 if __name__ == '__main__':
@@ -59,8 +71,8 @@ if __name__ == '__main__':
     init_params['seed'] = 6618115003047519509
     current = IndividualSuperFixedNormalDist(init_params=init_params)
 
-    baby = current.pair(other, pair_params=None)
+    baby = current.pair(other, pair_params={})
     visualize_strategies([current.value, other.value, baby.value])
     print(baby)
-    baby = baby.mutate(mutate_params=None)
+    baby = baby.mutate(mutate_params={})
     print(baby)
