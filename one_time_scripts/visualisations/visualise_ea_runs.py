8c2f9158e104793c34e080fdfe2c1d4f604d4be4
Jip Rietveld <jip_rietveld@hotmail.com>
2022-04-04 16:38:20 +0200

    Visualising multiple sets of runs

--- one_time_scripts/visualisations/visualise_ea_runs.py
+++ one_time_scripts/visualisations/visualise_ea_runs.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import csv
 
 
-def visualise_ea_run(filename=None):
+def convert_file_into_dict(filename=None):
     if filename is None:
         filename = '../../data/different_mutations/FixedNormalDistBigMutationWithSort.csv'
 
@@ -38,22 +38,56 @@ def visualise_ea_run(filename=None):
             last_generation = generation
         res_dict[f'run_{run}_best_individual'] = best_individuals_this_run
         res_dict[f'run_{run}_avg_individual'] = average_fitness_this_run
-        print(res_dict)
+        return res_dict, run + 1
 
-        colors = []
 
-        for i in range(run):
-            colors.append('#%06X' % random.randint(0, 0xFFFFFF))
+def visualise_ea_run(filename=None):
+    if filename is None:
+        filename = '../../data/different_mutations/FixedNormalDistBigMutationWithSort.csv'
 
-        for i in range(run):
-            color = colors[i]
-            plt.plot(res_dict[f'run_{i}_best_individual'], color=color, ls='-')
-            plt.plot(res_dict[f'run_{i}_avg_individual'], color=color, ls='--')
-        plt.title('Performance of 5 different runs with:')
-        plt.xlabel('Generation')
-        plt.ylabel('Performance (Total EUR)')
-        plt.show()
+    res_dict, num_of_runs = convert_file_into_dict(filename)
+    colors = []
+
+    for i in range(num_of_runs):
+        colors.append('#%06X' % random.randint(0, 0xFFFFFF))
+
+    for i in range(num_of_runs):
+        color = colors[i]
+        plt.plot(res_dict[f'run_{i}_best_individual'], color=color, ls='-')
+        plt.plot(res_dict[f'run_{i}_avg_individual'], color=color, ls='--')
+    plt.title('Performance of 5 different runs with:')
+    plt.xlabel('Generation')
+    plt.ylabel('Performance (Total EUR)')
+    plt.show()
+
+
+def visualise_ea_runs(filenames=None):
+    if filenames is None:
+        filenames = ['../../data/different_mutations/FixedNormalDistBigMutationWithSort.csv',
+                     '../../data/different_mutations/FixedNormalDistBigMutation.csv',
+                     '../../data/different_mutations/FixedNormalDistSmallMutation.csv']
+
+    all_dicts = []
+    colors = []
+    for filename in filenames:
+        res_dict, num_of_runs = convert_file_into_dict(filename)
+        all_dicts.append((res_dict, num_of_runs))
+
+        colors.append('#%06X' % random.randint(0, 0xFFFFFF))
+
+    for i in range(len(all_dicts)):
+        color = colors[i]
+        res_dict = all_dicts[i][0]
+        for j in range(all_dicts[i][1]):
+            plt.plot(res_dict[f'run_{j}_best_individual'], color=color, ls='-')
+            plt.plot(res_dict[f'run_{j}_avg_individual'], color=color, ls='--')
+    plt.title('Performance runs with:')
+    plt.xlabel('Generation')
+    plt.ylabel('Performance (Total EUR)')
+    plt.show()
 
 
 if __name__ == '__main__':
     visualise_ea_run()
+    visualise_ea_runs()
+
