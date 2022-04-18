93cf7969a48ed038ccba67fc2a6b38a09bd79c19
Jip Rietveld <jip_rietveld@hotmail.com>
2022-04-18 21:02:56 +0200

    finding the settings for the runs

--- evolutionary_algorithm/giga_baseline_main.py
+++ evolutionary_algorithm/giga_baseline_main.py
@@ -7,7 +7,7 @@ from evolutionary_algorithm.individuals.guided_initialisation.GuidedInitRandomNo
 from evolutionary_algorithm.individuals.mutation_params import big_mutation_with_overshoot
 
 
-def do_single_run(month=1, filename=None, pool_size=30, n_offsprings=15):
+def do_single_run(month=1, filename=None, pool_size=100, n_offsprings=50):
     number_of_points = 4
     price_step_size = 2
 
