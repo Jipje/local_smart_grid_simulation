8ff2a11f906a03fbfdf475f2a69831de7c362afa
Jip Rietveld <jip_rietveld@hotmail.com>
2022-04-25 08:16:20 +0200

    Added suffix and few_month handling

--- one_time_scripts/visualisations/baselines_visualisation.py
+++ one_time_scripts/visualisations/baselines_visualisation.py
@@ -12,6 +12,7 @@ month_long = ['january', 'february', 'march', 'april',
                 'september', 'october', 'november', 'december']
 baseline_df = pd.read_csv('../../data/baseline_earnings/overview.csv', delimiter=';')
 
+
 # 0                          Solarvation only discharging
 # 1     Wombat disregard congestion (with base money s...
 # 2             Wombat disregard congestion GIGA Baseline
@@ -26,19 +27,24 @@ baseline_df = pd.read_csv('../../data/baseline_earnings/overview.csv', delimiter
 # 11         Wombat max smart monthly timed GIGA Baseline
 # 12    Wombat avg smart monthly timed (with base mone...
 # 13         Wombat avg smart monthly timed GIGA Baseline
-def make_list_of_monthly_earnings(single_run):
+def make_list_of_monthly_earnings(single_run, few_months=None):
+    if few_months is None:
+        few_months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     res = []
-    for i in range(12):
+    for i in few_months:
         month_label = month_shorts[i] + '_earning'
         res.append(single_run[month_label])
     return res
 
 
-def make_list_of_monthly_earnings_from_ea_run_folder(source_folder='../../data/ea_runs/giga_baseline/'):
+def make_list_of_monthly_earnings_from_ea_run_folder(source_folder='../../data/ea_runs/giga_baseline/',
+                                                     suffix='', few_months=None):
+    if few_months is None:
+        few_months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     res_mean = []
     res_error = []
-    for i in range(12):
-        month_filename = source_folder + month_long[i] + '.csv'
+    for i in few_months:
+        month_filename = source_folder + month_long[i] + suffix + '.csv'
         dict_of_runs, num_of_runs = convert_file_into_dict(month_filename)
         arr_of_best_individuals = []
         for run_num in range(num_of_runs):
@@ -50,9 +56,14 @@ def make_list_of_monthly_earnings_from_ea_run_folder(source_folder='../../data/e
     return res_mean, res_error
 
 
-def make_bar_graph(baseline_indices, source_folders):
+def make_bar_graph(baseline_indices, source_folders, few_months=None, suffixes=None):
+    if few_months is None:
+        x_axis = np.array(list(range(10, 130, 10)))
+    else:
+        max_x = 10 + len(few_months) * 10
+        x_axis = np.array(list(range(10, max_x, 10)))
+
     num_of_items = len(baseline_indices) + len(source_folders)
-    x_axis = np.array(list(range(10, 130, 10)))
     offsets = []
     if num_of_items == 2:
         offsets = [-2, 2]
@@ -69,7 +80,7 @@ def make_bar_graph(baseline_indices, source_folders):
     offset_tracker = 0
     for i in range(len(baseline_indices)):
         single_run = baseline_df.loc[baseline_indices[i]]
-        single_run_y = make_list_of_monthly_earnings(single_run)
+        single_run_y = make_list_of_monthly_earnings(single_run, few_months)
 
         hatch = ''
         alpha = 1
@@ -81,14 +92,22 @@ def make_bar_graph(baseline_indices, source_folders):
                 hatch=hatch, alpha=alpha)
         offset_tracker = i
 
-    for source_folder in source_folders:
+    for source_folder_index in range(len(source_folders)):
+        source_folder = source_folders[source_folder_index]
+        if suffixes is not None:
+            assert len(suffixes) == len(source_folders), 'Please supply as many suffixes as folders'
+            suffix = suffixes[source_folder_index]
+        else:
+            suffix = ''
+
         offset_tracker = offset_tracker + 1
-        y_values, y_errors = make_list_of_monthly_earnings_from_ea_run_folder(source_folder)
+        y_values, y_errors = make_list_of_monthly_earnings_from_ea_run_folder(source_folder, suffix=suffix,
+                                                                              few_months=few_months)
         plt.bar(x_axis + offsets[offset_tracker], y_values, width, label=source_folder)
         plt.errorbar(x_axis + offsets[offset_tracker], y_values, yerr=y_errors,
-                 fmt='o', markersize=width, elinewidth=width*0.5)
+                     fmt='o', markersize=width, elinewidth=width*0.5)
 
-    plt.xticks(x_axis, month_shorts)
+    # plt.xticks(x_axis, month_shorts)
     plt.xlabel('Month (2021)')
     plt.ylabel('Total EUR')
     plt.title('Comparing monthly performance')
@@ -97,7 +116,16 @@ def make_bar_graph(baseline_indices, source_folders):
 
 
 if __name__ == '__main__':
-    label_indexes = [8, 9, 13]
+    # label_indexes = [8, 9, 13]
+    # source_folder_1 = '../../data/ea_runs/giga_baseline_with_congestion/'
+    # source_folder_2 = '../../data/ea_runs/random_init_first_runs/'
+    # make_bar_graph(label_indexes, source_folders=[source_folder_1, source_folder_2])
+
+    label_indexes = [8, 13]
     source_folder_1 = '../../data/ea_runs/giga_baseline_with_congestion/'
     source_folder_2 = '../../data/ea_runs/random_init_first_runs/'
-    make_bar_graph(label_indexes, source_folders=[source_folder_1, source_folder_2])
+    source_folder_3 = '../../data/ea_runs/sorting_investigation/'
+    source_folders = [source_folder_1, source_folder_2, source_folder_3]
+    all_suffix = ['', '', '_sort_1']
+    only_april = [3]
+    make_bar_graph(label_indexes, source_folders=source_folders, suffixes=all_suffix, few_months=only_april)
