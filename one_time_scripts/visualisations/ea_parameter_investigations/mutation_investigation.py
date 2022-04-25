96d54f8abef4335176f477cec8e7632e6f91641c
Jip Rietveld <jip_rietveld@hotmail.com>
2022-04-25 11:35:05 +0200

    Added random mutation to the investigation

--- one_time_scripts/visualisations/ea_parameter_investigations/mutation_investigation.py
+++ one_time_scripts/visualisations/ea_parameter_investigations/mutation_investigation.py
@@ -2,11 +2,14 @@ from one_time_scripts.visualisations.visualise_ea_runs import visualise_ea_runs
 
 if __name__ == '__main__':
     filenames_march = ['../../../data/ea_runs/mutation_investigation/march_big_mutation.csv',
-                       '../../../data/ea_runs/mutation_investigation/march_big_mutation_with_overshoot.csv']
+                       '../../../data/ea_runs/mutation_investigation/march_big_mutation_with_overshoot.csv',
+                       '../../../data/ea_runs/mutation_investigation/march_random_mutation.csv']
     filenames_april = ['../../../data/ea_runs/mutation_investigation/april_big_mutation.csv',
-                       '../../../data/ea_runs/mutation_investigation/april_big_mutation_with_overshoot.csv']
+                       '../../../data/ea_runs/mutation_investigation/april_big_mutation_with_overshoot.csv',
+                       '../../../data/ea_runs/mutation_investigation/april_random_mutation.csv']
     filenames_november = ['../../../data/ea_runs/mutation_investigation/november_big_mutation.csv',
-                          '../../../data/ea_runs/mutation_investigation/november_big_mutation_with_overshoot.csv']
+                          '../../../data/ea_runs/mutation_investigation/november_big_mutation_with_overshoot.csv',
+                          '../../../data/ea_runs/mutation_investigation/november_random_mutation.csv']
     filenames_all = [filenames_march, filenames_april, filenames_november]
     base_title = 'Mutation investigation '
     titles = [base_title + 'March', base_title + 'April', base_title + 'November']
