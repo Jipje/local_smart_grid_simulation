57219a4bfea60f77e60a59a43ec3779772e9ce3d
Jip Rietveld <jip_rietveld@hotmail.com>
2022-05-05 16:07:25 +0200

    Setting up first big runs

--- offspring_ratio_main.py
+++ offspring_ratio_main.py
@@ -8,90 +8,23 @@ from evolutionary_algorithm.individuals.guided_initialisation.GuidedInitRandomNo
     GuidedInitRandomNormalDist
 from evolutionary_algorithm.individuals.mutation_params import big_mutation_with_overshoot, random_mutation, \
     big_mutation
+from evolutionary_algorithm.runnable_main import default_ea_runnable_settings, do_default_run
 
 month_filenames = ['january', 'february', 'march', 'april',
                            'may', 'june', 'july', 'august',
                            'september', 'october', 'november', 'december']
 
+run_settings_1 = default_ea_runnable_settings
+run_settings_1['pop_size'] = 100
+run_settings_1['n_offsprings'] = 40
 
-def do_fully_random_single_run(month=1, filename=None, pool_size=100, n_offsprings=50):
-    number_of_points = 4
-    price_step_size = 2
+run_settings_2 = default_ea_runnable_settings
+run_settings_2['pop_size'] = 100
+run_settings_2['n_offsprings'] = 80
 
-    fitness_class = Fitness()
-    fitness_class.set_month(month)
-    mutate_params = random_mutation
-    mutate_params['strategy_price_step_size'] = price_step_size
-    mutate_params['sort_strategy'] = None
-
-    evo = Evolution(
-        pool_size=pool_size,
-        fitness=fitness_class.fitness,
-        individual_class=IndividualRandomNormalDist,
-        n_offsprings=n_offsprings,
-        pair_params={'strategy_price_step_size': price_step_size},
-        mutate_params=mutate_params,
-        init_params={
-            'number_of_points': number_of_points,
-            'strategy_price_step_size': price_step_size
-        }
-    )
-    n_epochs = 40
-
-    if filename is None:
-        filename = month_filenames[month - 1]
-
-    for _ in range(n_epochs):
-        evo.step()
-        evo.report()
-        evo.write_to_csv(f'..{os.path.sep}data{os.path.sep}ea_runs{os.path.sep}offspring_ratio{os.path.sep}{filename}.csv')
-        # evo.write_to_csv(f'data{os.path.sep}ea_runs{os.path.sep}population_investigation{os.path.sep}{filename}.csv')
-        if evo.early_end():
-            break
-
-    print(f'Best performing individual {filename}:\tElite fitness: {evo.pool.individuals[-1].fitness}\n')
-    print(evo.pool.individuals[-1])
-
-
-def do_single_run(month=1, filename=None, mutate_params=None):
-    number_of_points = 4
-    price_step_size = 2
-
-    fitness_class = Fitness()
-    fitness_class.set_month(month)
-    if mutate_params is None:
-        mutate_params = big_mutation_with_overshoot
-    mutate_params['strategy_price_step_size'] = price_step_size
-    mutate_params['sort_strategy'] = None
-
-    evo = Evolution(
-        pool_size=100,
-        fitness=fitness_class.fitness,
-        individual_class=IndividualRandomNormalDist,
-        n_offsprings=50,
-        pair_params={'strategy_price_step_size': price_step_size},
-        mutate_params=mutate_params,
-        init_params={
-            'number_of_points': number_of_points,
-            'strategy_price_step_size': price_step_size
-        }
-    )
-    n_epochs = 40
-
-    if filename is None:
-        filename = month_filenames[month - 1]
-
-    for _ in range(n_epochs):
-        evo.step()
-        evo.report()
-        # evo.write_to_csv(f'..{os.path.sep}data{os.path.sep}ea_runs{os.path.sep}mutation_investigation{os.path.sep}{filename}.csv')
-        evo.write_to_csv(f'data{os.path.sep}ea_runs{os.path.sep}mutation_investigation{os.path.sep}{filename}.csv')
-        if evo.early_end():
-            break
-
-    print(f'Best performing individual {filename}:\tElite fitness: {evo.pool.individuals[-1].fitness}\n')
-    print(evo.pool.individuals[-1])
-
+run_settings_3 = default_ea_runnable_settings
+run_settings_3['pop_size'] = 100
+run_settings_3['n_offsprings'] = 160
 
 if __name__ == '__main__':
     try:
@@ -101,16 +34,22 @@ if __name__ == '__main__':
 
     if runnable_int == 1:
         print('Running setting 1')
-        for _ in range(4):
+        for _ in range(5):
             for month_index in [3, 4, 11]:
                 month_filename = month_filenames[month_index - 1]
-                custom_filename = month_filename + f'_big_mutation_with_overshoot'
-                do_single_run(month=month_index, filename=custom_filename)
+                custom_filename = month_filename + '_40_over_100'
+                do_default_run(run_settings_1, month=month_index, filename=custom_filename, folder='offspring_ratio')
+    elif runnable_int == 2:
+        print('Running setting 2')
+        for _ in range(5):
+            for month_index in [3, 4, 11]:
+                month_filename = month_filenames[month_index - 1]
+                custom_filename = month_filename + '_160_over_100'
+                do_default_run(run_settings_2, month=month_index, filename=custom_filename, folder='offspring_ratio')
     else:
         print('Running setting other')
-        for _ in range(4):
+        for _ in range(5):
             for month_index in [3, 4, 11]:
                 month_filename = month_filenames[month_index - 1]
-                custom_filename = month_filename + f'_big_mutation'
-                custom_mutate_params = big_mutation
-                do_single_run(month=month_index, filename=custom_filename, mutate_params=custom_mutate_params)
+                custom_filename = month_filename + '_80_over_100'
+                do_default_run(run_settings_3, month=month_index, filename=custom_filename, folder='offspring_ratio')
