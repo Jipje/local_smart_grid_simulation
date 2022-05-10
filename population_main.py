23840d56f3b760a8a6e138e8a9dac5ed30632b98
Jip Rietveld <jip_rietveld@hotmail.com>
2022-05-10 15:05:18 +0200

    Correct folder implementation

--- population_main.py
+++ population_main.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     except IndexError:
         runnable_int = 0
 
-    folder = ''
+    folder = 'population'
 
     if runnable_int == 1:
         print('Running setting 1')
@@ -31,14 +31,14 @@ if __name__ == '__main__':
             for month_index in [3, 4, 11]:
                 month_filename = month_filenames[month_index - 1]
                 custom_filename = month_filename + '_16_over_20'
-                do_an_ea_run(run_settings_1, month=month_index, filename=custom_filename, folder='offspring_ratio')
+                do_an_ea_run(run_settings_1, month=month_index, filename=custom_filename, folder=folder)
     elif runnable_int == 2:
         print('Running setting 2')
         for _ in range(4):
             for month_index in [3, 4, 11]:
                 month_filename = month_filenames[month_index - 1]
                 custom_filename = month_filename + '_32_over_40'
-                do_an_ea_run(run_settings_2, month=month_index, filename=custom_filename, folder='offspring_ratio')
+                do_an_ea_run(run_settings_2, month=month_index, filename=custom_filename, folder=folder)
     else:
         print('Running setting other')
         print(run_settings_3)
@@ -46,4 +46,4 @@ if __name__ == '__main__':
             for month_index in [3, 4, 11]:
                 month_filename = month_filenames[month_index - 1]
                 custom_filename = month_filename + '_160_over_200'
-                do_an_ea_run(run_settings_3, month=month_index, filename=custom_filename, folder='offspring_ratio')
+                do_an_ea_run(run_settings_3, month=month_index, filename=custom_filename, folder=folder)
