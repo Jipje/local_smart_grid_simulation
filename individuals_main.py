6966f818869e996dbe6b83be3e83e825ae576b26
Jip Rietveld <jip_rietveld@hotmail.com>
2022-05-12 08:43:40 +0200

    Created main to investigate individual

--- individuals_main.py
+++ individuals_main.py
@@ -1,54 +1,18 @@
-import os
 import sys
 
-from evolutionary_algorithm.Evolution import Evolution
-from evolutionary_algorithm.Fitness import Fitness
 from evolutionary_algorithm.individuals.IndividualMutateNormalDist import IndividualMutateNormalDist
 from evolutionary_algorithm.individuals.IndividualRandomNormalDist import IndividualRandomNormalDist
-from evolutionary_algorithm.individuals.mutation_params import random_mutation
+from evolutionary_algorithm.runnable_main import default_ea_runnable_settings, do_an_ea_run
 
+month_filenames = ['january', 'february', 'march', 'april',
+                   'may', 'june', 'july', 'august',
+                   'september', 'october', 'november', 'december']
 
-def do_single_run(individual_class, month=1, filename=None, label='RandomNormalDist'):
-    number_of_points = 4
-    price_step_size = 2
+run_settings_1 = default_ea_runnable_settings
+run_settings_1['individual_class'] = IndividualRandomNormalDist
 
-    fitness_class = Fitness()
-    fitness_class.set_month(month)
-    mutate_params = random_mutation
-    mutate_params['strategy_price_step_size'] = price_step_size
-    mutate_params['sort_strategy'] = 1
-
-    evo = Evolution(
-        pool_size=100,
-        fitness=fitness_class.fitness,
-        individual_class=individual_class,
-        n_offsprings=50,
-        pair_params={'strategy_price_step_size': price_step_size},
-        mutate_params=mutate_params,
-        init_params={
-            'number_of_points': number_of_points,
-            'strategy_price_step_size': price_step_size
-        }
-    )
-    n_epochs = 40
-
-    if filename is None:
-        month_filenames = ['january', 'february', 'march', 'april',
-                           'may', 'june', 'july', 'august',
-                           'september', 'october', 'november', 'december']
-        filename = month_filenames[month - 1] + f'_{label}'
-
-    for _ in range(n_epochs):
-        evo.step()
-        evo.report()
-        # evo.write_to_csv(f'..{os.path.sep}data{os.path.sep}ea_runs{os.path.sep}individual_investigation{os.path.sep}{filename}.csv')
-        evo.write_to_csv(f'data{os.path.sep}ea_runs{os.path.sep}individual_investigation{os.path.sep}{filename}.csv')
-        if evo.early_end():
-            break
-
-    print(f'Best performing individual {filename}:\tElite fitness: {evo.pool.individuals[-1].fitness}\n')
-    print(evo.pool.individuals[-1])
-
+run_settings_2 = default_ea_runnable_settings
+run_settings_1['individual_class'] = IndividualMutateNormalDist
 
 if __name__ == '__main__':
     try:
@@ -56,17 +20,19 @@ if __name__ == '__main__':
     except IndexError:
         runnable_int = 0
 
+    folder = 'individual'
+
     if runnable_int == 1:
         print('Running setting 1')
-        for _ in range(6):
+        for _ in range(4):
             for month_index in [3, 4, 11]:
-                other_individual = IndividualRandomNormalDist
-                other_label = 'RandomNormalDist'
-                do_single_run(month=month_index, individual_class=other_individual, label=other_label)
+                month_filename = month_filenames[month_index - 1]
+                custom_filename = month_filename + '_double_dist'
+                do_an_ea_run(run_settings_1, month=month_index, filename=custom_filename, folder=folder)
     else:
         print('Running setting other')
-        for _ in range(6):
+        for _ in range(4):
             for month_index in [3, 4, 11]:
-                other_individual = IndividualMutateNormalDist
-                other_label = 'MutateNormalDist'
-                do_single_run(month=month_index, individual_class=other_individual, label=other_label)
+                month_filename = month_filenames[month_index - 1]
+                custom_filename = month_filename + '_single_dist'
+                do_an_ea_run(run_settings_2, month=month_index, filename=custom_filename, folder=folder)
