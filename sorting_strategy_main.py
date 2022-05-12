c5df7a85e701d474e063130a74cc49f9ce0cbb99
Jip Rietveld <jip_rietveld@hotmail.com>
2022-05-12 08:48:04 +0200

    Adjusted sorting strategy main

--- sorting_strategy_main.py
+++ sorting_strategy_main.py
@@ -1,54 +1,57 @@
-import os
+import sys
 
-from evolutionary_algorithm.Evolution import Evolution
-from evolutionary_algorithm.Fitness import Fitness
-from evolutionary_algorithm.individuals.IndividualRandomNormalDist import IndividualRandomNormalDist
-from evolutionary_algorithm.individuals.mutation_params import big_mutation_with_overshoot
+from evolutionary_algorithm.runnable_main import default_ea_runnable_settings, do_an_ea_run
 
+month_filenames = ['january', 'february', 'march', 'april',
+                   'may', 'june', 'july', 'august',
+                   'september', 'october', 'november', 'december']
 
-def do_single_run(month=1, filename=None, sort_strategy=None):
-    number_of_points = 4
-    price_step_size = 2
+run_settings_1 = default_ea_runnable_settings
+run_settings_1['sort_strategy'] = None
 
-    fitness_class = Fitness()
-    fitness_class.set_month(month)
-    mutate_params = big_mutation_with_overshoot
-    mutate_params['strategy_price_step_size'] = price_step_size
-    mutate_params['sort_strategy'] = sort_strategy
+run_settings_2 = default_ea_runnable_settings
+run_settings_2['sort_strategy'] = 1
 
-    evo = Evolution(
-        pool_size=100,
-        fitness=fitness_class.fitness,
-        individual_class=IndividualRandomNormalDist,
-        n_offsprings=50,
-        pair_params={'strategy_price_step_size': price_step_size},
-        mutate_params=mutate_params,
-        init_params={
-            'number_of_points': number_of_points,
-            'strategy_price_step_size': price_step_size
-        }
-    )
-    n_epochs = 40
+run_settings_3 = default_ea_runnable_settings
+run_settings_3['sort_strategy'] = 2
 
-    if filename is None:
-        month_filenames = ['january', 'february', 'march', 'april',
-                           'may', 'june', 'july', 'august',
-                           'september', 'october', 'november', 'december']
-        filename = month_filenames[month - 1] + f'_sort_{sort_strategy}'
-
-    for _ in range(n_epochs):
-        evo.step()
-        evo.report()
-        # evo.write_to_csv(f'..{os.path.sep}data{os.path.sep}ea_runs{os.path.sep}sorting_investigation{os.path.sep}{filename}.csv')
-        evo.write_to_csv(f'data{os.path.sep}ea_runs{os.path.sep}sorting_investigation{os.path.sep}{filename}.csv')
-        if evo.early_end():
-            break
-
-    print(f'Best performing individual {filename}:\tElite fitness: {evo.pool.individuals[-1].fitness}\n')
-    # print(evo.pool.individuals[-1])
+run_settings_4 = default_ea_runnable_settings
+run_settings_4['sort_strategy'] = 3
 
 
 if __name__ == '__main__':
-    for _ in range(2):
-        for month_index in [3, 4, 11]:
-            do_single_run(month=month_index, sort_strategy=3)
+    try:
+        runnable_int = int(sys.argv[1])
+    except IndexError:
+        runnable_int = 0
+
+    folder = 'sorting'
+
+    if runnable_int == 1:
+        print('Running setting 1')
+        for _ in range(4):
+            for month_index in [3, 4, 11]:
+                month_filename = month_filenames[month_index - 1]
+                custom_filename = month_filename + '_no_sort'
+                do_an_ea_run(run_settings_1, month=month_index, filename=custom_filename, folder=folder)
+    elif runnable_int == 2:
+        print('Running setting 2')
+        for _ in range(4):
+            for month_index in [3, 4, 11]:
+                month_filename = month_filenames[month_index - 1]
+                custom_filename = month_filename + '_sort_1'
+                do_an_ea_run(run_settings_1, month=month_index, filename=custom_filename, folder=folder)
+    elif runnable_int == 3:
+        print('Running setting 3')
+        for _ in range(4):
+            for month_index in [3, 4, 11]:
+                month_filename = month_filenames[month_index - 1]
+                custom_filename = month_filename + '_sort_2'
+                do_an_ea_run(run_settings_1, month=month_index, filename=custom_filename, folder=folder)
+    else:
+        print('Running setting other')
+        for _ in range(4):
+            for month_index in [3, 4, 11]:
+                month_filename = month_filenames[month_index - 1]
+                custom_filename = month_filename + '_sort_3'
+                do_an_ea_run(run_settings_2, month=month_index, filename=custom_filename, folder=folder)
