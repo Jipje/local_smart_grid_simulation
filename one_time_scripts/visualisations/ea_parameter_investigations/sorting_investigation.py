ba1eb09fdd570d64f1426748b5c7c51dbea7479f
Jip Rietveld <jip_rietveld@hotmail.com>
2022-05-25 13:38:34 +0200

    Adding sorting investigation visualisations

--- one_time_scripts/visualisations/ea_parameter_investigations/sorting_investigation.py
+++ one_time_scripts/visualisations/ea_parameter_investigations/sorting_investigation.py
@@ -1,19 +1,19 @@
-from one_time_scripts.visualisations.baselines_visualisation import make_bar_graph
+from one_time_scripts.visualisations.baselines_visualisation import make_bar_graph, statistic_tests
 from one_time_scripts.visualisations.visualise_ea_runs import visualise_ea_runs
 
 if __name__ == '__main__':
-    filenames_march = ['../../../data/ea_runs/sorting_investigation/march_sort_none.csv',
-                       '../../../data/ea_runs/sorting_investigation/march_sort_1.csv',
-                       '../../../data/ea_runs/sorting_investigation/march_sort_2.csv',
-                       '../../../data/ea_runs/sorting_investigation/march_sort_3.csv']
-    filenames_april = ['../../../data/ea_runs/sorting_investigation/april_sort_none.csv',
-                       '../../../data/ea_runs/sorting_investigation/april_sort_1.csv',
-                       '../../../data/ea_runs/sorting_investigation/april_sort_2.csv',
-                       '../../../data/ea_runs/sorting_investigation/april_sort_3.csv']
-    filenames_nov = ['../../../data/ea_runs/sorting_investigation/november_sort_none.csv',
-                     '../../../data/ea_runs/sorting_investigation/november_sort_1.csv',
-                     '../../../data/ea_runs/sorting_investigation/november_sort_2.csv',
-                     '../../../data/ea_runs/sorting_investigation/november_sort_3.csv']
+    filenames_march = ['../../../data/new_ea_runs/sorting/march_no_sort.csv',
+                       '../../../data/new_ea_runs/sorting/march_sort_1.csv',
+                       '../../../data/new_ea_runs/sorting/march_sort_2.csv',
+                       '../../../data/new_ea_runs/sorting/march_sort_3.csv']
+    filenames_april = ['../../../data/new_ea_runs/sorting/april_no_sort.csv',
+                       '../../../data/new_ea_runs/sorting/april_sort_1.csv',
+                       '../../../data/new_ea_runs/sorting/april_sort_2.csv',
+                       '../../../data/new_ea_runs/sorting/april_sort_3.csv']
+    filenames_nov = ['../../../data/new_ea_runs/sorting/november_no_sort.csv',
+                     '../../../data/new_ea_runs/sorting/november_sort_1.csv',
+                     '../../../data/new_ea_runs/sorting/november_sort_2.csv',
+                     '../../../data/new_ea_runs/sorting/november_sort_3.csv']
     filenames_all = [filenames_march, filenames_april, filenames_nov]
     base_title = 'Sorting investigation '
     titles = [base_title + 'March', base_title + 'April', base_title + 'November']
@@ -23,10 +23,18 @@ if __name__ == '__main__':
         visualise_ea_runs(filenames, title)
 
     label_indexes = [13]
-    source_folder_1 = '../../../data/ea_runs/giga_baseline_with_congestion/'
-    source_folder_3 = '../../../data/ea_runs/sorting_investigation/'
-    source_folders = [source_folder_1, source_folder_3, source_folder_3, source_folder_3, source_folder_3]
-    all_suffix = ['', '_sort_none', '_sort_1', '_sort_2', '_sort_3']
+    source_folder = '../../../data/new_ea_runs/sorting/'
+    source_folders = [source_folder, source_folder, source_folder, source_folder]
+    all_suffix = ['_no_sort', '_sort_1', '_sort_2', '_sort_3']
     few_months = [2, 3, 10]
     make_bar_graph(label_indexes, source_folders=source_folders, suffixes=all_suffix, few_months=few_months,
-                   num_of_source_folder_baselines=1)
+                   num_of_source_folder_baselines=0)
+
+    statistic_tests([], [source_folder, source_folder], few_months=few_months,
+                    suffixes=['_sort_1', '_no_sort'])
+
+    # statistic_tests([], [source_folder, source_folder], few_months=few_months,
+    #                 suffixes=['_sort_1', '_sort_2'])
+
+    statistic_tests([], [source_folder, source_folder], few_months=few_months,
+                    suffixes=['_sort_1', '_sort_3'])
